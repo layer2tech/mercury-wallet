85948968276c3fb2620e24e7f314b00e9b67db27
tomos <tomos@commerceblock.com>
2020-11-05 17:49:22 +0100

    Clean and rm old code

--- app/src/features/CoinDataSlice.js
+++ app/src/features/CoinDataSlice.js
@@ -1,13 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { nanoid } from '@reduxjs/toolkit'
-
-// A slice is a section of store data. Here we define its reducers functions.
-// Actions are auto-generated and exported here.
 
 const initialState = [
   { id: "1", amount: "0.005", time_left: "11.5 months", address: "bc5f...1lp4" }
 ]
-
 
 const CoinSlice = createSlice({
   name: 'coinData',
--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -3,6 +3,5 @@ import storedDataReducer from '../features/StoredDataSlice'
 import coinDataReducer from '../features/CoinDataSlice'
 
 export default combineReducers({
-  storedData: storedDataReducer,
   coinData: coinDataReducer,
 })
