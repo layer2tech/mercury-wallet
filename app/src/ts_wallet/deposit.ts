77de04b54276f2ef437291b1ad19e5c7d0b1e9bc
tomos <tomos@commerceblock.com>
2020-11-26 16:42:25 +0100

    Include typescript

---
+++ app/src/ts_wallet/deposit.ts
@@ -0,0 +1,133 @@
+import { POST_ROUTE, post } from "./request";
+
+export const deposit = async () => {
+  let secret_key = "12345";
+  let proof_key = "02851ad2219901fc72ea97b4d21e803c625a339f07da8c7069ea33ddd0125da84f";
+  let value = 10;
+  let protocol = "Deposit";
+
+  let shared_key_id = await despoitInit(proof_key);
+
+  let master_key = await keyGen(shared_key_id, secret_key, proof_key, value, protocol);
+
+  let message = "1111";
+  let sign_msg = await sign(shared_key_id, master_key, message, protocol);
+
+  console.log("signature: ", sign_msg)
+}
+
+
+export const despoitInit = async (proof_key: string) => {
+    let deposit_msg1 = {
+        auth: "authstr",
+        proof_key: String(proof_key)
+    }
+
+    let shared_key_id = await post(POST_ROUTE.DEPOSIT_INIT, deposit_msg1);
+
+    return shared_key_id
+}
+
+
+export const keyGen = async (
+    shared_key_id: string,
+    secret_key: string,
+    _proof_key: string,
+    value: number,
+    protocol: string
+  ) => {
+  // Import Rust functions
+  let wasm = await import('client-wasm');
+
+  let keygen_msg1 = {
+      shared_key_id: shared_key_id,
+      protocol: protocol,
+  };
+  // server first
+  let [id, kg_party_one_first_message] = await post(POST_ROUTE.KEYGEN_FIRST, keygen_msg1);
+
+  //client first
+  let {kg_party_two_first_message, kg_ec_key_pair_party2} =
+    JSON.parse(
+      wasm.KeyGen.first_message(secret_key)
+    );
+
+  // server second
+  let key_gen_msg2 = {
+    shared_key_id: shared_key_id,
+    dlog_proof:kg_party_two_first_message.d_log_proof,
+  }
+  let kg_party_one_second_message = await post(POST_ROUTE.KEYGEN_SECOND, key_gen_msg2);
+
+  // client second
+  let {party_two_second_message, party_two_paillier} =
+    JSON.parse(
+      wasm.KeyGen.second_message(
+        JSON.stringify(kg_party_one_first_message),
+        JSON.stringify(kg_party_one_second_message)
+      )
+    );
+
+  // Construct Rust MasterKey struct
+  let master_key =
+    JSON.parse(
+      wasm.KeyGen.set_master_key(
+        JSON.stringify(kg_ec_key_pair_party2),
+        JSON.stringify(kg_party_one_second_message
+                .ecdh_second_message
+                .comm_witness
+                .public_share),
+        JSON.stringify(party_two_paillier)
+    ));
+
+    return master_key
+}
+
+// message should be hex string
+export const sign = async (
+  shared_key_id: string,
+  master_key: any,
+  message: string,
+  protocol: string
+) => {
+  // Import Rust functions
+  let wasm = await import('client-wasm');
+
+  //client first
+  let {eph_key_gen_first_message_party_two, eph_comm_witness, eph_ec_key_pair_party2} =
+    JSON.parse(
+      wasm.Sign.first_message()
+    );
+
+  // server first
+  let sign_msg1 = {
+      shared_key_id: shared_key_id,
+      eph_key_gen_first_message_party_two,
+  };
+  let sign_party_one_first_message = await post(POST_ROUTE.SIGN_FIRST, sign_msg1);
+
+  //client second
+  let party_two_sign_message =
+    JSON.parse(
+      wasm.Sign.second_message(
+        JSON.stringify(master_key),
+        JSON.stringify(eph_ec_key_pair_party2),
+        JSON.stringify(eph_comm_witness),
+        JSON.stringify(sign_party_one_first_message),
+        message
+      )
+    );
+
+  let sign_msg2 = {
+      shared_key_id: shared_key_id,
+      sign_second_msg_request: {
+          protocol,
+          message,
+          party_two_sign_message,
+      },
+  };
+
+  let signature = await post(POST_ROUTE.SIGN_SECOND, sign_msg2);
+
+  return signature
+}
--- app/src/wallet/wallet.js
+++ app/src/wallet/wallet.js
@@ -1,103 +1,127 @@
-let bitcoin = require('bitcoinjs-lib')
-let bip32utils = require('bip32-utils')
-let bip32 = require('bip32')
-let bip39 = require('bip39')
-const fsLibrary  = require('fs')
-
-const WALLET_LOC = "wallet.json";
-
-function Wallet(mnemonic, account) {
-  this.mnemonic = mnemonic
-  this.account = account
-}
-
-// Constructors
-Wallet.fromMnemonic = function (mnemonic) {
-  return new Wallet(mnemonic, Wallet.mnemonic_to_bip32_root_account(mnemonic))
-}
-
-// Take mnemonic phrase String and return bip32utils.Account Object.
-Wallet.mnemonic_to_bip32_root_account = function (mnemonic) {
-  if (!bip39.validateMnemonic(mnemonic)) {
-    return "Invalid mnemonic"
-  }
-  const seed = bip39.mnemonicToSeedSync(mnemonic);
-  const root = bip32.fromSeed(seed);
-
-  let i = root.deriveHardened(0)
-  let external = i.derive(0)
-  let internal = i.derive(1)
-
-  // Bip32 Account is made up of two Bip32 Chains.
-  let account = new bip32utils.Account([
-    new bip32utils.Chain(external, null, segwitAddr),
-    new bip32utils.Chain(internal, null, segwitAddr)
-  ])
-  return account
-}
-
-// Convert bip32utils.Account to json and write to file.
-Wallet.prototype.save = function (file_path) {
-  // Store in file as JSON string
-  fsLibrary.writeFile(file_path, JSON.stringify(this), (error) => {
-    if (error) throw err;
-  })
-};
-
-// Read storage file and parse bip32utils.Account from json.
-// Copied from bip32-utils. Library version not compatible with newer bitcoinjs-lib versions.
-Wallet.load = async (file_path, network, addressFunction) => {
-
-  // Fetch raw json
-  let json_wallet = await new Promise((resolve,reject) => {
-        fsLibrary.readFile(file_path, (error, txtString) => {
-          if (error) throw err;
-          resolve(txtString.toString())
-        });
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  json_wallet = JSON.parse(json_wallet);
-
-  // Re-derive Account from JSON
-  const chains = json_wallet.account.map(function (j) {
-    const node = bip32.fromBase58(j.node, network)
-
-    const chain = new bip32utils.Chain(node, j.k, addressFunction)
-    chain.map = j.map
-
-    chain.addresses = Object.keys(chain.map).sort(function (a, b) {
-      return chain.map[a] - chain.map[b]
-    })
-
-    return chain
-  })
-
-  let account = new bip32utils.Account(chains)
-  return new Wallet(json_wallet.mnemonic, account)
-}
-
-// Getters
-Wallet.prototype.displayAccount = function () {
-  console.log(this.account)
-}
-
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var bitcoin = require('bitcoinjs-lib');
+var bip32utils = require('bip32-utils');
+var bip32 = require('bip32');
+var bip39 = require('bip39');
+var fsLibrary = require('fs');
+var WALLET_LOC = "wallet.json";
+// Wallet holds BIP32 key root and derivation progress information.
+var Wallet = /** @class */ (function () {
+    function Wallet(mnemonic, account) {
+        var _this = this;
+        this.save = function (file_path) {
+            // Store in file as JSON string
+            fsLibrary.writeFile(file_path, JSON.stringify(_this), function (error) {
+                if (error)
+                    throw error;
+            });
+        };
+        // Getters
+        this.displayAccount = function () {
+            console.log(_this.account);
+        };
+        this.mnemonic = mnemonic;
+        this.account = account;
+    }
+    // Constructors
+    Wallet.fromMnemonic = function (mnemonic) {
+        return new Wallet(mnemonic, mnemonic_to_bip32_root_account(mnemonic));
+    };
+    Wallet.load = function (file_path, network, addressFunction) { return __awaiter(void 0, void 0, void 0, function () {
+        var str_wallet, json_wallet, chains, account;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, new Promise(function (resolve, _reject) {
+                        fsLibrary.readFile(file_path, function (error, txtString) {
+                            if (error)
+                                throw error;
+                            resolve(txtString.toString());
+                        });
+                    })];
+                case 1:
+                    str_wallet = _a.sent();
+                    json_wallet = JSON.parse(str_wallet);
+                    chains = json_wallet.account.map(function (j) {
+                        var node = bip32.fromBase58(j.node, network);
+                        var chain = new bip32utils.Chain(node, j.k, addressFunction);
+                        chain.map = j.map;
+                        chain.addresses = Object.keys(chain.map).sort(function (a, b) {
+                            return chain.map[a] - chain.map[b];
+                        });
+                        return chain;
+                    });
+                    account = new bip32utils.Account(chains);
+                    return [2 /*return*/, new Wallet(json_wallet.mnemonic, account)];
+            }
+        });
+    }); };
+    return Wallet;
+}());
+// BIP39 mnemonic -> BIP32 Account
+var mnemonic_to_bip32_root_account = function (mnemonic) {
+    if (!bip39.validateMnemonic(mnemonic)) {
+        return "Invalid mnemonic";
+    }
+    var seed = bip39.mnemonicToSeedSync(mnemonic);
+    var root = bip32.fromSeed(seed);
+    var i = root.deriveHardened(0);
+    var external = i.derive(0);
+    var internal = i.derive(1);
+    // BIP32 Account is made up of two BIP32 Chains.
+    var account = new bip32utils.Account([
+        new bip32utils.Chain(external, null, segwitAddr),
+        new bip32utils.Chain(internal, null, segwitAddr)
+    ]);
+    return account;
+};
 // Address generation fn
-const segwitAddr = (node) => {
-  const p2wpkh = bitcoin.payments.p2wpkh({
-    pubkey: node.publicKey,
-    network: bitcoin.networks.bitcoin
-  })
-  return p2wpkh.address
-}
-
-
-const mnemonic =
-  'praise you muffin lion enable neck grocery crumble super myself license ghost';
-
+var segwitAddr = function (node) {
+    var p2wpkh = bitcoin.payments.p2wpkh({
+        pubkey: node.publicKey,
+        network: bitcoin.networks.bitcoin
+    });
+    return p2wpkh.address;
+};
+var mnemonic = 'praise you muffin lion enable neck grocery crumble super myself license ghost';
 var wallet = Wallet.fromMnemonic(mnemonic);
-console.log("Wallet: ", wallet)
-
-wallet.save(WALLET_LOC)
-
-let json = Wallet.load(WALLET_LOC).then(json => {
-  console.log("json: ",json)
+wallet.save(WALLET_LOC);
+Wallet.load(WALLET_LOC, bitcoin.networks.bitcoin, segwitAddr).then(function (json) {
+    console.log("json: ", json);
 });
