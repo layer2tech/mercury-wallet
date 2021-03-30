96f7706669c676db95a6a918a81704a1dcaeaa2b
alexyjs <alexyjs1@gmail.com>
2021-03-30 19:45:27 +0300

    toggle show password

--- src/components/createWizardForm/createWizardForm.js
+++ src/components/createWizardForm/createWizardForm.js
@@ -1,12 +1,23 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState } from 'react';
 import {useForm} from "react-hook-form";
 
 import './createWizardForm.css'
 
+const EyeIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/></svg>
+);
+
+const EyeIconOff = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0z" fill="none"/><path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z"/></svg>
+);
+
 const CreateWizardForm = (props) => {
     const {register, errors, watch, handleSubmit} = useForm({mode: 'onChange', reValidateMode: 'onChange',});
+    const [showPass, setShowPass] = useState(false);
     const password = useRef({});
     password.current = watch("password", "");
+
+    const toggleShowPass = () => setShowPass(!showPass);
 
     function onSubmit(data) {
         props.onSubmit()
@@ -26,7 +37,7 @@ const CreateWizardForm = (props) => {
                 </div>
 
                 <div className="inputs-item">
-                  <input id="Passphrase" type="password" name="password"
+                  <input id="Passphrase" type={showPass ? 'text' : 'password'} name="password"
                     placeholder="Passphrase (min 8 characters)"
                     onChange={props.setStateWalletPassword}
                     ref={register({
@@ -36,20 +47,24 @@ const CreateWizardForm = (props) => {
                        }
                     })}
                     />
-
+                    <span className={'eye-icon'} onClick={toggleShowPass}>
+                        {showPass ? <EyeIconOff /> : <EyeIcon />}
+                    </span>
                 </div>
                 <div className="error">
                     {errors.password && <p>{errors.password.message}</p>}
                 </div>
 
                 <div className="inputs-item">
-                  <input id="password_repeat"type="password" name="password_repeat"
+                  <input id="password_repeat" type={showPass ? 'text': 'password'} name="password_repeat"
                     placeholder="Confirm Passphrase"
                     ref={register({
                        validate: value =>
                            value===password.current || "The passwords do not match"
                     })}/>
-
+                    <span className={'eye-icon'} onClick={toggleShowPass}>
+                        {showPass ? <EyeIconOff /> : <EyeIcon />}
+                    </span>
                 </div>
                 <div className="error">
                     {errors.password_repeat && <p>{errors.password_repeat.message}</p>}
