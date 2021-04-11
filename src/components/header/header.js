e643e23fe58eacd604aef2f3217fab58d7a591a8
alexyjs <alexyjs1@gmail.com>
2021-04-11 21:34:13 +0300

    update app header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import logo from '../../images/monochrome - white@4x.png';
-import question from '../../images/header-question.png';
-import settings from '../../images/settings-icon.png';
 import { Link, withRouter } from "react-router-dom";
-import { NotificationBar, ErrorPopup } from "../../components";
+import { NotificationBar, ErrorPopup, ConfirmPopup } from "../../components";
+import { Logo, Settings, Help, Logout} from './headerIcons';
+import { unloadWallet } from '../../features/WalletDataSlice'
 import './header.css';
 
 const Header = (props) => {
+  const handleLogout = () => {
+    unloadWallet();
+    props.setWalletLoaded(false);
+  }
   return (
     <div className="Header">
 
       <div className="container block">
         <Link className="navbar-brand" to={props.walletLoaded ? "/home" : "/"}>
-          <img src={logo} className="Header-logo" alt="logo" />
+          <Logo />
         </Link>
 
         <div className="menu">
           <div className={`nav-item  ${props.location.pathname === "/" ? "active" : ""}`}>
             <Link className="nav-link" to="/help">
-              <img src={question} alt="question" />
+              <Help />
             </Link>
           </div>
 
           {props.walletLoaded ?
             <div className={`nav-item  ${props.location.pathname === "/settings" ? "active" : ""}`}>
               <Link className="nav-link" to="/settings">
-                <img src={settings} alt="settings" />
+                <Settings />
               </Link>
             </div>
             :
             null
           }
+          {props.walletLoaded && (
+            <div className={`nav-item`}>
+              <ConfirmPopup onOk={handleLogout}>
+                <div className="header-logout">
+                  <Logout />
+                </div>
+              </ConfirmPopup>
+            </div>
+          )}
         </div>
       </div>
       <NotificationBar />
