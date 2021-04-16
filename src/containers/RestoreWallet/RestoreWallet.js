02817aca32b864bbc0e865349a786f16262c28be
alexyjs <alexyjs1@gmail.com>
2021-04-16 07:00:59 +0300

    add toggle to show password for restore wallet page

--- src/containers/RestoreWallet/RestoreWallet.js
+++ src/containers/RestoreWallet/RestoreWallet.js
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom";
 import { useDispatch } from 'react-redux'
 
 import {setError, walletFromMnemonic} from '../../features/WalletDataSlice'
+import eyeIcon from "../../images/eye-icon.svg";
+import eyeIconOff from "../../images/eye-icon-off.svg";
 
 import  './RestoreWallet.css'
 
@@ -11,6 +13,7 @@ let Store = window.require('electron-store');
 let store = new Store();
 
 const RestoreWalletPage = (props) => {
+  const [showPass, setShowPass] = useState(false);
   const dispatch = useDispatch();
 
   const [state, setState] = useState(
@@ -19,6 +22,7 @@ const RestoreWalletPage = (props) => {
       wallet_password: "",
       mnemonic: "",
     });
+  const toggleShowPass = () => setShowPass(!showPass);
   const setStateWalletName = (event) => setState({...state, wallet_name: event.target.value});
   const setStateWalletPassword = (event) => setState({...state, wallet_password: event.target.value});
   const setStateMnemonic = (event) => setState({...state, mnemonic: event.target.value});
@@ -52,7 +56,15 @@ const RestoreWalletPage = (props) => {
       </div>
 
       <div className="inputs-item">
-          <input type="password" name="wallet_password" required placeholder="Password " onChange={setStateWalletPassword}/>
+          <input 
+            type={showPass ? 'text' : 'password'} name="password"
+            name="wallet_password" 
+            required placeholder="Password " 
+            onChange={setStateWalletPassword}
+          />
+          <span className={'eye-icon'} onClick={toggleShowPass}>
+              {showPass ? <img src={eyeIconOff} /> : <img src={eyeIcon} />}
+          </span>
       </div>
 
       <div >
