f87f2083626781dd308025b5ca9d4af38c68cc50
Lawrence Deacon <lawrence.deacon@gmail.com>
2021-04-28 15:58:26 +0100

    Shorten tor test

--- src/wallet/test/tor.test.js
+++ src/wallet/test/tor.test.js
@@ -6,5 +6,4 @@ const tor = new Tor('localhost', 9050, 'lawrence', 9051);
 
 test('tor-rp', async function() {
     await tor.confirmNewTorConnection();
-    await tor.confirmNewTorConnection();
 });
