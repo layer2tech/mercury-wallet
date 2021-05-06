8a6f2d6fe875715d6124009a1ca348b07fb96596
Lawrence Deacon <lawrence.deacon@gmail.com>
2021-05-06 15:58:50 +0100

    Tor server config

--- src/wallet/config.ts
+++ src/wallet/config.ts
@@ -22,7 +22,8 @@ export class Config {
     ip: string,
     port: number,
     controlPassword: string,
-    controlPort: number
+    controlPort: number,
+    serverPort: number,
   };
 
   min_anon_set: number;
@@ -47,7 +48,8 @@ export class Config {
       ip: 'localhost',
       port: 9050,
       controlPassword: 'password',
-      controlPort: 9051
+      controlPort: 9051,
+      serverPort: 3001,
     };
 
     this.min_anon_set = 5;
@@ -67,38 +69,58 @@ export class Config {
     Object.entries(config_changes).forEach((item) => {
       switch(item[0]) {
         case "network":
-          return;
+          break;
         case "testing_mode":
           this.testing_mode = item[1];
-          return;
+          break;
         case "jest_testing_mode":
           this.jest_testing_mode = item[1];
-          return;
+          break;
         case "required_confirmations":
           this.required_confirmations = item[1];
-          return;
+          break;
 
         case "state_entity_endpoint":
           this.state_entity_endpoint = item[1];
-          return;
+          break;
         case "swap_conductor_endpoint":
           this.swap_conductor_endpoint = item[1];
-          return;
+          break;
         case "electrum_config":
           this.electrum_config = item[1];
-          return;
+          break;
         case "tor_proxy":
-          this.tor_proxy = item[1];
-          return;
+          for (var tp_item in item[1]) {
+            switch(tp_item[0]){
+                case "ip":
+                    this.tor_proxy.ip = tp_item[1];
+                    break;
+                case "port":
+                    this.tor_proxy.port = tp_item[1];
+                    break;
+                case "controlPassword":
+                    this.tor_proxy.controlPassword = tp_item[1];
+                    break;
+                case "controlPort":
+                    this.tor_proxy.controlPort = tp_item[1];
+                    break;
+                case "serverPort":
+                    this.tor_proxy.serverPort = tp_item[1];
+                    break;
+                default: 
+                  throw Error("Config tor_proxy entry "+tp_item[0]+" does not exist")
+            }
+          };
+          break;
         case "min_anon_set":
           this.min_anon_set = item[1];
-          return;
+          break;
         case "notifications":
           this.notifications = item[1]
-          return;
+          break;
         case "tutorials":
           this.tutorials = item[1]
-          return;
+          break;
         default:
           throw Error("Config entry "+item[0]+" does not exist")
       }
--- tor/server/index.js
+++ tor/server/index.js
@@ -1,38 +1,64 @@
 var TorClient = require('./tor_client');
+var Config = new require('./config');
+const config = new Config();
+const tpc = config.tor_proxy;
 const express = require("express");
+var cors = require('cors')
 
-const PORT = process.env.PORT || 3001;
+
+const PORT = config.tor_proxy.serverPort;
 
 const app = express();
+//app.use(cors({
+//  origin: 'http://localhost:3000'
+//}));
 
-password='tijf5631';
-
-const tor = new TorClient('localhost', 9050, password, 9051, 'http://zo63hfpdcmonu52pcvflmeo62s47cqdabmibeejm7bhrfxmino3fl5qd.onion');
+const tor = new TorClient(tpc.ip, tpc.port, tpc.controlPassword, tpc.controlPort);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get('*', async function(req,res) {
+async function get_endpoint(req, res, endpoint){
   try{
     //console.log(JSON.stringify(req));
-    let result = await tor.get(req.path,undefined);
+    let result = await tor.get(req.path,undefined, endpoint);
     res.json(result);
   } catch (err){
     console.log("get err: "+ err);
     res.status(err.statusCode).json(err);
   }
-});
+};
 
-app.post('*', async function(req,res) {
+async function post_endpoint(req, res, endpoint) {
   try{
-    let result = await tor.post(req.path,req.body);
+    let result = await tor.post(req.path,req.body, endpoint);
     console.log("result: ")
     res.json(result);
   } catch (err) {
     console.log("post err: "+ err);
     res.status(err.statusCode).json(err);
   }
+};
+
+app.get('*', async function(req,res) {
+  get_endpoint(req, res, config.state_entity_endpoint)
 });
+
+app.post('*', async function(req,res) {
+  post_endpoint(req, res, config.state_entity_endpoint)
+});
+
+
+app.get('/swap/*', async function(req,res) {
+ get_endpoint(req, res, config.swap_conductor_endpoint)
+});
+
+app.post('/swap/*', async function(req,res) {
+  post_endpoint(req, res, config.swap_conductor_endpoint)
+});
+
+
+
 
 
