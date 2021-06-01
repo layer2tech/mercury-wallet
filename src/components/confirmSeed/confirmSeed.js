0dc51328719acee4f597dc0cf32d2d1ce2879d07
RajKadir <r.kadir@se16.qmul.ac.uk>
2021-06-01 16:21:25 +0100

    chore: revert and remove format from previous

--- src/components/confirmSeed/confirmSeed.js
+++ src/components/confirmSeed/confirmSeed.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { withRouter } from "react-router-dom";
-import { useDispatch } from 'react-redux'
-import { setError, walletFromMnemonic, callGetVersion, callGetUnspentStatecoins } from '../../features/WalletDataSlice'
+import {useDispatch} from 'react-redux'
+import {setError, walletFromMnemonic, callGetVersion, callGetUnspentStatecoins} from '../../features/WalletDataSlice'
 
 import './confirmSeed.css'
 
@@ -22,32 +22,32 @@ const ConfirmSeed = (props) => {
   );
 
   let words = props.wizardState.mnemonic.split(" ");
-  const [missingwords, setMissingWords] = useState(() => rands.map((rand) => ({ pos: rand, word: "" })));
+  const [missingwords, setMissingWords] = useState(() => rands.map((rand) => ({pos:rand, word:""})));
 
   const inputMissingWord = (event) => {
-    let map = missingwords.map((item) => { if (item.pos === parseInt(event.target.id)) { item.word = event.target.value } return item })
+    let map = missingwords.map((item) => {if (item.pos===parseInt(event.target.id)) {item.word=event.target.value} return item})
     setMissingWords(map)
   }
 
   // Display other words and create input boxes
   let words_without_missing = words.map((item, index) => (rands.includes(index) ? "" : item))
   const inputs = words_without_missing.map((item, index) => (
-    <input key={index}
-      id={index}
-      type="text"
-      placeholder={index + 1 + ". " + item}
-      value={item === '' ? missingwords.find((item) => { if (item.pos === index) { return item } return null }).word : ""}
-      disabled={item === '' ? "" : "disabled"}
-      onChange={inputMissingWord} />
+      <input key={index}
+        id={index}
+        type="text"
+        placeholder={index + 1 + ". " + item}
+        value={item === '' ? missingwords.find((item) => {if (item.pos===index) {return item} return null}).word : ""}
+        disabled={item === '' ? "" : "disabled"}
+        onChange={inputMissingWord}/>
   ))
 
   // Confirm words are correct
   const onConfirmClick = (event) => {
     // Verify mnemonic confirmation
-    for (let i = 0; i < missingwords.length; i++) {
-      if (missingwords[i].word !== words[missingwords[i].pos]) {
+    for (let i=0;i<missingwords.length; i++) {
+      if (missingwords[i].word!==words[missingwords[i].pos]) {
         event.preventDefault();
-        dispatch(setError({ msg: "Seed confirmation failed." }))
+        dispatch(setError({msg: "Seed confirmation failed."}))
         return
       }
     }
@@ -57,9 +57,9 @@ const ConfirmSeed = (props) => {
     const checkForCoinsHealth = () => {
       let unspent_coins_data = callGetUnspentStatecoins();
       let coins_data = unspent_coins_data[0];
-      for (let i = 0; i < coins_data.length; i++) {
+      for (let i=0; i<coins_data.length; i++) {
         if (coins_data[i].wallet_version !== callGetVersion()) {
-          dispatch(setError({ msg: "Warning: Coin in wallet was created in previous wallet version. Due to rapid development some backward incompatible changes may break old coins. We recommend withdrawing testnet coins and creating a fresh wallet." }))
+          dispatch(setError({msg: "Warning: Coin in wallet was created in previous wallet version. Due to rapid development some backward incompatible changes may break old coins. We recommend withdrawing testnet coins and creating a fresh wallet."}))
           break;
         };
       }
@@ -71,26 +71,26 @@ const ConfirmSeed = (props) => {
       props.history.push('/home')
     } catch (e) {
       event.preventDefault();
-      dispatch(setError({ msg: e.message }))
+      dispatch(setError({msg: e.message}))
     }
     checkForCoinsHealth();
     props.setWalletLoaded(true);
   }
 
   return (
-    <div className="wizard-form-confirm wizard-form inputs">
-      <p>Click below or type in the missing words to confirm your seed key.</p>
+      <div className="wizard-form-confirm wizard-form inputs">
+          <p>Click below or type in the missing words to confirm your seed key.</p>
 
-      <form>
-        {inputs}
-      </form>
-      <div className="footer-step-btns">
-        <button onClick={props.onPrevStep} className="primary-btn-link back">Go back</button>
-        <button className="primary-btn blue" onClick={onConfirmClick} >
-          Confirm
+          <form>
+              {inputs}
+          </form>
+          <div className="footer-step-btns">
+            <button onClick={props.onPrevStep} className="primary-btn-link back">Go back</button>
+            <button className="primary-btn blue" onClick={onConfirmClick} >
+                Confirm
             </button>
+          </div>
       </div>
-    </div>
   )
 }
 
