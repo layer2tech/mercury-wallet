0f3abb5082a9bf630c5cdb63864b713794382218
Lawrence Deacon <lawrence.deacon@gmail.com>
2021-06-25 16:27:03 +0100

    e2e test for restore a wallet from memory

--- src/e2e/e2e.test.js
+++ src/e2e/e2e.test.js
@@ -35,9 +35,38 @@ test('Restore a wallet from seed', async t => {
     await t
         .typeText(restore_form.child(0),'clinic shrug sock pumpkin cotton useful wise word solid enlist fix that')
         //.click(restore_form.child('Name'));
-        .typeText(restore_form.child(1),'test restore seed wallet')
+        .typeText(restore_form.child(1),'test_restore_seed_wallet')
         .typeText(restore_form.child(3),'password')
         .typeText(restore_form.child(5),'password')
         .click(restore_form.child('.checkbox'))
         .click(restore_form.child('.footer-step-btns').child(1));
+});
+
+test('Restore a wallet from memory', async t => {
+    const restore_button = Selector('.welcome-btns')
+        .child('div')
+        .nth(2);
+
+    const continue_button = Selector('.welcome-first')
+        .child('.send');
+  
+    await t
+        .click(restore_button)
+        .click(continue_button);
+        
+
+    const restore_form = Selector('.memory-form')
+        .child('form')
+        .child('div');
+        //.child('.wizard-form')
+        //.child(0);
+        //.child('div')
+        //.child('div');
+
+    //const input_elements = restore_form.find('input');
+    //console.log(`input elements: ${restore_form}`);
+
+    await t
+        .typeText(restore_form.child(2),'password')
+        .click(restore_form.child('.footer-btns').child(1));
 });
