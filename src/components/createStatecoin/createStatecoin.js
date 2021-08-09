bac98495f453e0d6f64d2727c1942daf53906f4c
RajKadir <r.kadir@se16.qmul.ac.uk>
2021-08-09 14:28:06 +0100

    chore: fix  coin  info

--- src/components/createStatecoin/createStatecoin.js
+++ src/components/createStatecoin/createStatecoin.js
@@ -2,7 +2,7 @@ import '../../containers/Deposit/Deposit.css';
 import React, {useState, useEffect} from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 import plus from "../../images/plus-deposit.png";
-import {callGetFeeInfo} from '../../features/WalletDataSlice';
+import {callGetCoinsInfo, callGetFeeInfo} from '../../features/WalletDataSlice';
 import ValueSelectionPanel from "./valueSelection/valueSelection";
 import { FEE, MINIMUM_DEPOSIT_SATOSHI, fromSatoshi } from '../../wallet/util';
 
@@ -24,67 +24,72 @@ const CreateStatecoin = (props) => {
     }
 
     useEffect(() => {
-      // Update liquidity data state
-      let liquidity_data = Object.entries(DEFAULT_LIQUIDITY_VALUES.values).map(([amount, liquidity]) => {
-        return {value: parseInt(amount), liquidity: liquidity}
-      })
-
-      // Add list of defualt values if not already in list
-      let liquidity_data_amounts = liquidity_data.map((item) => item.value);
-      let defaults_missing = DEFAULT_LIQUIDITY_VALUES.filter((item) => {
-        // checks if default value is already in liquidity_data. If not return item.
-        if (liquidity_data_amounts.indexOf(item.value)<0) return item;
-      });
-      liquidity_data=liquidity_data.concat(defaults_missing)
-
-      // Sort
-      if (props.settings.sort_by==="Liquidity") {
-        liquidity_data.sort((a,b) => {
-          return b.liquidity - a.liquidity;
+      // Get coin liquidity data
+      callGetCoinsInfo().then((liquidity_data_raw) => {
+        // Update liquidity data state
+        let liquidity_data = Object.entries(liquidity_data_raw.values).map(([amount, liquidity]) => {
+          return {value: parseInt(amount), liquidity: liquidity}
         })
-      } else { // sort by amount
-        liquidity_data.sort((a,b) => {
-          if(props.settings.sort_by === "HighToLow") {
-            return b.value - a.value;
-          }
-          return a.value - b.value;
+
+        // Add list of defualt values if not already in list
+        let liquidity_data_amounts = liquidity_data.map((item) => item.value);
+        let defaults_missing = DEFAULT_LIQUIDITY_VALUES.filter((item) => {
+          // checks if default value is already in liquidity_data. If not return item.
+          if (liquidity_data_amounts.indexOf(item.value)<0) return item;
+        });
+        liquidity_data=liquidity_data.concat(defaults_missing)
+
+        // Sort
+        if (props.settings.sort_by==="Liquidity") {
+          liquidity_data.sort((a,b) => {
+            return b.liquidity - a.liquidity;
+          })
+        } else { // sort by amount
+          liquidity_data.sort((a,b) => {
+            if(props.settings.sort_by === "HighToLow") {
+              return b.value - a.value;
+            }
+            return a.value - b.value;
+          })
+        }
+
+        // Replace liquidity value with string "None", "Low", "Med" or "High"
+        let num_highs=0;
+        liquidity_data.map((item) => {
+          if (!item.liquidity) {item.liquidityLabel="None"}
+          else if (item.liquidity<LIQUIDITY_MED) {item.liquidityLabel="Low"}
+          else if (item.liquidity<LIQUIDITY_HIGH) {item.liquidityLabel="Med"}
+          else {
+            if (num_highs<NUM_HIGH_LIQUIDITY) { // Only allow top 3 values to have "high" liquidity
+              item.liquidityLabel="High";
+              num_highs+=1;
+            } else {
+              item.liquidityLabel="Med";
+            }
+          };
+          return item;
         })
-      }
 
-      // Replace liquidity value with string "None", "Low", "Med" or "High"
-      let num_highs=0;
-      liquidity_data.map((item) => {
-        if (!item.liquidity) {item.liquidityLabel="None"}
-        else if (item.liquidity<LIQUIDITY_MED) {item.liquidityLabel="Low"}
-        else if (item.liquidity<LIQUIDITY_HIGH) {item.liquidityLabel="Med"}
-        else {
-          if (num_highs<NUM_HIGH_LIQUIDITY) { // Only allow top 3 values to have "high" liquidity
-            item.liquidityLabel="High";
-            num_highs+=1;
-          } else {
-            item.liquidityLabel="Med";
-          }
-        };
-        return item;
-      })
+        // Filter by min
+        liquidity_data = liquidity_data.filter(item => fromSatoshi(item.value) >= props.settings.min_value)
+        // truncate to display top {settings.picks} choices
+        liquidity_data = liquidity_data.slice(0, props.settings.picks)
 
-      // Filter by min
-      liquidity_data = liquidity_data.filter(item => fromSatoshi(item.value) >= props.settings.min_value)
-      // truncate to display top {settings.picks} choices
-      liquidity_data = liquidity_data.slice(0, props.settings.picks)
-
-      // filter out coins where the value is not greater than the total fee
-      callGetFeeInfo().then(fee =>  {
-        liquidity_data = liquidity_data.filter(statecoin => statecoin.value >= (FEE + ((statecoin.value * fee.withdraw) / 10000)));
-        // ensure coins cannot be below 0.002 btc
-        liquidity_data = liquidity_data.filter(statecoin => statecoin.value >= MINIMUM_DEPOSIT_SATOSHI);
-        setLiquidityData(liquidity_data);
-        setLoading(false);
+        // filter out coins where the value is not greater than the total fee
+        callGetFeeInfo().then(fee =>  {
+          liquidity_data = liquidity_data.filter(statecoin => statecoin.value >= (FEE + ((statecoin.value * fee.withdraw) / 10000)));
+          setLiquidityData(liquidity_data);
+          setLoading(false);
+        }).catch(e => {
+          setError({error: true, message: 'Failed retrieving fee info from server...'});
+          setLoading(false);
+        });
       }).catch(e => {
-        setError({error: true, message: 'Failed retrieving fee info from server...'});
         setLoading(false);
+        setError({error: true, message: 'Failed retrieving statecoin values from server...'});
       });
     }, [props.settings]);
+
 
     const populateWithSelectionPanels = props.selectedValues.map((item, index) => (
         <div key={index}>
