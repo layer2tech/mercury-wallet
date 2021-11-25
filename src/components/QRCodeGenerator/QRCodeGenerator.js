6215f654284e5ae6932ce6120fb6ba71f879a98a
rk16449 <32778886+rk16449@users.noreply.github.com>
2021-11-25 23:03:07 +0000

    chore: add outline around qr box (#947)

--- src/components/QRCodeGenerator/QRCodeGenerator.js
+++ src/components/QRCodeGenerator/QRCodeGenerator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './qrcode.css'
 let QRCode = require('qrcode.react');
 
 //Generates a BTC QR code from a p address and an amount passed through as props
@@ -9,9 +9,11 @@ const QRCodeGenerator = (props) => {
     const makeQRCodeString = (address, amount) => "bitcoin:"+address+"?amount="+amount;
 
     return(
+      <div class='qr-code'>
         <QRCode value={makeQRCodeString(props.address, props.amount)}
         level='H'
       />
+      </div>
     )
 }
 
