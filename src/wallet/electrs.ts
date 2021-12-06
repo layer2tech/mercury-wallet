bef3cf1740476e2999701d4f261f218d832b50d3
Lawrence Deacon <lawrence.deacon@gmail.com>
2021-12-06 16:20:57 +0000

    Remove typo

--- src/wallet/electrs.ts
+++ src/wallet/electrs.ts
@@ -178,7 +178,7 @@ export class ElectrsClient {
     return blockHeight
     } catch(err) {
       this.blockHeightLatest = null
-      callBack([{"height":null}])q
+      callBack([{"height":null}])
       let err_str = typeof err === 'string' ? err : err?.message
       if (err_str && err_str.includes('Network Error')){
         return null
