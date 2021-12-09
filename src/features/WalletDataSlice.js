97a572e8145772611e1ef9a0a3447826984b122f
Sam Gershuny <samgershuny@gmail.com>
2021-12-09 11:14:10 +0000

    Deleting unused functons

--- src/features/WalletDataSlice.js
+++ src/features/WalletDataSlice.js
@@ -253,10 +253,6 @@ export const callGetUnconfirmedStatecoinsDisplayData = () => {
   return wallet.getUnconfirmedStatecoinsDisplayData()
 }
 
-export const callAddActivityItem = (id, action) =>{
-  wallet.addActivityItem(id, action);
-}
-
 export const callGetActivityLog = () => {
   return wallet.getActivityLog();
 }
