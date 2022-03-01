ee2afd7add0514f2141aa53fc78949a44f055058
RajKadir <r.kadir@se16.qmul.ac.uk>
2022-03-01 15:29:08 +0000

    chore: add fix for duplicate activity objects based off date

--- src/components/activity/activity.js
+++ src/components/activity/activity.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Moment from 'react-moment';
 import { fromSatoshi } from '../../wallet/util'
-import { callGetActivityLogItems,callGetAllStatecoins } from '../../features/WalletDataSlice'
+import { callGetActivityLogItems, callGetAllStatecoins } from '../../features/WalletDataSlice'
 import txidIcon from '../../images/txid-icon.png';
 import createIcon from '../../images/create-icon-dep.png';
 import transferIcon from '../../images/transfer-icon.png';
@@ -11,26 +11,26 @@ import EmptyCoinDisplay from '../coins/EmptyCoinDisplay/EmptyCoinDisplay';
 import './activity.css';
 
 const Activity = () => {
-    let activity_data = callGetActivityLogItems(10);
+	let activity_data = callGetActivityLogItems(10);
 	let statecoins = callGetAllStatecoins();
 
-	function shortenString(long){
+	function shortenString(long) {
 		let short = ""
-		short = short.concat(long.slice(0,6),"...")
+		short = short.concat(long.slice(0, 6), "...")
 		return short
 	}
 
-	function swapTxid(funding_txid, date){
-		let dateIndexArray = activity_data.filter(item => item.funding_txid === funding_txid && item.action === "S" )
+	function swapTxid(funding_txid, date) {
+		let dateIndexArray = activity_data.filter(item => item.funding_txid === funding_txid && item.action === "S")
 		// Filter activity for txid and swap actions
 
-		dateIndexArray = dateIndexArray.map(item => item.date).sort((a,b) => b-a)
+		dateIndexArray = dateIndexArray.map(item => item.date).sort((a, b) => b - a)
 		//sort filtered activity by date (most recent to least recent)
 
 		let dateIndex = dateIndexArray.indexOf(date)
 		// Get index of swapped coin
-		
-		let swappedCoins = statecoins.filter(coin => coin.funding_txid === funding_txid && coin.status === "SWAPPED").sort((a,b) => a.date - b.date).reverse()
+
+		let swappedCoins = statecoins.filter(coin => coin.funding_txid === funding_txid && coin.status === "SWAPPED").sort((a, b) => a.date - b.date).reverse()
 		// Filter all statecoins for swapped TxID and sort by date (most recent to least recent)
 
 		// Check data exists : some unforeseen error
@@ -38,25 +38,25 @@ const Activity = () => {
 		let datedSwappedCoins = swappedCoins[dateIndex];
 		let finalTxid = datedSwappedCoins ? datedSwappedCoins?.swap_transfer_finalized_data?.state_chain_data?.utxo?.txid :
 			undefined;
-		if(finalTxid){
+		if (finalTxid) {
 			return shortenString(finalTxid)
 		}
-		else{
+		else {
 			return "Data not found"
 		}
 	}
 
-	if(activity_data?.length) {
+	if (activity_data?.length) {
 		activity_data = activity_data.sort((a, b) => {
 			return b?.date - a?.date;
 		})
 	}
 	const mergeActivityByDate = () => {
-		if(!activity_data.length) return [];
+		if (!activity_data.length) return [];
 		let allActivity = {};
 		activity_data.forEach(item => {
 			let dateStr = new Date(item.date).toLocaleDateString();
-			if(allActivity.hasOwnProperty(dateStr)) {
+			if (allActivity.hasOwnProperty(dateStr)) {
 				allActivity[dateStr].push(item);
 			} else {
 				allActivity[dateStr] = [item];
@@ -65,161 +65,174 @@ const Activity = () => {
 		return Object.values(allActivity);
 	};
 
-	const activityDataMergeDate = mergeActivityByDate();
-    const activitiesTableData = activityDataMergeDate.map((activityGroup, groupIndex) =>(
-        <div key={groupIndex}>
-            <div className="date">
-                <Moment format="MMMM D, YYYY">{activityGroup[0]?.date}</Moment>
-            </div>
-			{activityGroup.map((item, index) => (			
+	let activityDataMergeDate = mergeActivityByDate();
+
+	{
+		/* remove duplicates from array */
+		console.table(activityDataMergeDate);
+
+		// if the dates are exactly the same then remove them
+		activityDataMergeDate = activityDataMergeDate.filter((element, index, self) =>
+			index === self.findIndex((t) => (
+				t.date === element.date
+			))
+		)
+	}
+
+	const activitiesTableData = activityDataMergeDate.map((activityGroup, groupIndex) => (
+		<div key={groupIndex}>
+			<div className="date">
+				<Moment format="MMMM D, YYYY">{activityGroup[0]?.date}</Moment>
+			</div>
+			{activityGroup.map((item, index) => (
 				<div key={index}>
 
 					{item.action === 'D' ?
 						<table>
 							<tbody>
-							<tr >
-								<td>
-									<img src={createIcon} alt="createIcon"/>
-									<span> <Moment format="HH:mm A">{item.date}</Moment> </span>
-									<span className="black">Created 1 Statecoin</span>
-								</td>
-								<td>
-									<img src={txidIcon} alt="txidIcon"/>
-									<span className="txid">{item.funding_txid}:{item.funding_txvout}</span>
+								<tr >
+									<td>
+										<img src={createIcon} alt="createIcon" />
+										<span> <Moment format="HH:mm:ss A">{item.date}</Moment> </span>
+										<span className="black">Created 1 Statecoin</span>
+									</td>
+									<td>
+										<img src={txidIcon} alt="txidIcon" />
+										<span className="txid">{item.funding_txid}:{item.funding_txvout}</span>
 
-								</td>
-								<td>
-									<span>Deposited</span>
-									<span className="green">+ {fromSatoshi(item.value)} BTC</span>
+									</td>
+									<td>
+										<span>Deposited</span>
+										<span className="green">+ {fromSatoshi(item.value)} BTC</span>
 
-								</td>
-							</tr>
+									</td>
+								</tr>
 							</tbody>
 						</table>
-					: null }
+						: null}
 					{item.action === 'T' ?
 						<table>
 							<tbody>
-							<tr >
-								<td>
-									<img src={transferIcon} alt="transferIcon"/>
-									<span><Moment format="HH:mm A">{item.date}</Moment> </span>
-									<span className="black">Transferred 1 Statecoin</span>
-								</td>
-								<td>
-									<img src={txidIcon} alt="txidIcon"/>
-									<span className="txid">{item.funding_txid}:{item.funding_txvout}</span>
+								<tr >
+									<td>
+										<img src={transferIcon} alt="transferIcon" />
+										<span><Moment format="HH:mm:ss A">{item.date}</Moment> </span>
+										<span className="black">Transferred 1 Statecoin</span>
+									</td>
+									<td>
+										<img src={txidIcon} alt="txidIcon" />
+										<span className="txid">{item.funding_txid}:{item.funding_txvout}</span>
 
-								</td>
-								<td>
-									<span>Transferred</span>
-									<span className="red">- {fromSatoshi(item.value)} BTC</span>
-								</td>
-							</tr>
+									</td>
+									<td>
+										<span>Transferred</span>
+										<span className="red">- {fromSatoshi(item.value)} BTC</span>
+									</td>
+								</tr>
 							</tbody>
 						</table>
-					: null }
+						: null}
 					{item.action === 'W' ?
-					<table>
-						<tbody>
-							<tr >
+						<table>
+							<tbody>
+								<tr >
 
-								<td>
-									<img src={withrowIcon} alt="withrowIcon"/>
-									<span className="grey"><Moment format="HH:mm A">{item.date}</Moment> </span>
-									<span >Withdrawn 1 Statecoin</span>
-								</td>
-								<td>
-									<img src={txidIcon} alt="txidIcon"/>
-									<span className="txid">{item.funding_txid}:{item.funding_txvout}</span>
-
-								</td>
-								<td>
-									<span>Withdrawn</span>
-									<span className="red">- {fromSatoshi(item.value)} BTC</span>
-
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					: null }
-					{item.action === 'S' ?
-					<table className = "swap-row-table" >
-
-						<tbody>
-							<span className = "tooltip" >
-								<div><b>New TxID: </b>{swapTxid(item.funding_txid,item.date)}:{item.funding_txvout}</div>
-								<div><b>Swapped TxId: </b>{shortenString(item.funding_txid)}:{item.funding_txvout}</div>
-							</span>
-							<tr>
-								<td>
-									<img src={withrowIcon} alt="withrowIcon"/>
-									<span className="grey"><Moment format="HH:mm A">{item.date}</Moment> </span>
-									<span >Swapped 1 Statecoin</span>
-								</td>
-								<td>
-									<img src={swapIcon} alt="txidIcon"/>
-									<span className="txid">{item.funding_txid}:{item.funding_txvout}</span>
-
-								</td>
-								<td>
-									<span>Swapped</span>
-									<span className="green"> {fromSatoshi(item.value)} BTC</span>
-
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					: null }
-					{item.action === 'I'?
-					<table>
-					<tbody>
-						<tr >
-							<td>
-								<img src={withrowIcon} alt="createIcon"/>
-								<span><Moment format="HH:mm A">{item.date}</Moment> </span>
-								<span >Initialized 1 Statecoin</span>
-							</td>
-							<td>
-								<span className="">No Transaction ID before Deposit</span>
-							</td>
-							<td>
-								<span>Coin Initialized</span>
-							</td>
-						</tr>
-					</tbody>
-					</table>
-					:null}
-					{item.action === 'R'?
-    				<table>
-						<tbody>
-							<tr>
-								<td>
-									<img src={withrowIcon} alt="receiveIcon"/>
-									<span><Moment format="HH:mm A">{item.date}</Moment> </span>
-									<span >Received 1 Statecoin</span>
-								</td>
 									<td>
-										<img src={txidIcon} alt="txidIcon"/>
+										<img src={withrowIcon} alt="withrowIcon" />
+										<span className="grey"><Moment format="HH:mm:ss A">{item.date}</Moment> </span>
+										<span >Withdrawn 1 Statecoin</span>
+									</td>
+									<td>
+										<img src={txidIcon} alt="txidIcon" />
+										<span className="txid">{item.funding_txid}:{item.funding_txvout}</span>
+
+									</td>
+									<td>
+										<span>Withdrawn</span>
+										<span className="red">- {fromSatoshi(item.value)} BTC</span>
+
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						: null}
+					{item.action === 'S' ?
+						<table className="swap-row-table" >
+
+							<tbody>
+								<span className="tooltip" >
+									<div><b>New TxID: </b>{swapTxid(item.funding_txid, item.date)}:{item.funding_txvout}</div>
+									<div><b>Swapped TxId: </b>{shortenString(item.funding_txid)}:{item.funding_txvout}</div>
+								</span>
+								<tr>
+									<td>
+										<img src={withrowIcon} alt="withrowIcon" />
+										<span className="grey"><Moment format="HH:mm:ss A">{item.date}</Moment> </span>
+										<span >Swapped 1 Statecoin</span>
+									</td>
+									<td>
+										<img src={swapIcon} alt="txidIcon" />
+										<span className="txid">{item.funding_txid}:{item.funding_txvout}</span>
+
+									</td>
+									<td>
+										<span>Swapped</span>
+										<span className="green"> {fromSatoshi(item.value)} BTC</span>
+
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						: null}
+					{item.action === 'I' ?
+						<table>
+							<tbody>
+								<tr >
+									<td>
+										<img src={withrowIcon} alt="createIcon" />
+										<span><Moment format="HH:mm:ss A">{item.date}</Moment> </span>
+										<span >Initialized 1 Statecoin</span>
+									</td>
+									<td>
+										<span className="">No Transaction ID before Deposit</span>
+									</td>
+									<td>
+										<span>Coin Initialized</span>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						: null}
+					{item.action === 'R' ?
+						<table>
+							<tbody>
+								<tr>
+									<td>
+										<img src={withrowIcon} alt="receiveIcon" />
+										<span><Moment format="HH:mm:ss A">{item.date}</Moment> </span>
+										<span >Received 1 Statecoin</span>
+									</td>
+									<td>
+										<img src={txidIcon} alt="txidIcon" />
 										<span className="txid">{item.funding_txid}:{item.funding_txvout}</span>
 									</td>
 									<td>
 										<span>Coin Received</span>
 										<span className="green">+ {fromSatoshi(item.value)} BTC</span>
 									</td>
-							</tr>
-						</tbody>
-					</table>
-					:null}
-			</div>
+								</tr>
+							</tbody>
+						</table>
+						: null}
+				</div>
 			))}
-        </div>
-    ))
-    return (
-        <div >
-			{!activity_data.length? (<EmptyCoinDisplay message="No activity recorded."/>):(activitiesTableData)}
+		</div>
+	))
+	return (
+		<div >
+			{!activity_data.length ? (<EmptyCoinDisplay message="No activity recorded." />) : (activitiesTableData)}
 
-        </div>
-    )
+		</div>
+	)
 }
 export default Activity
