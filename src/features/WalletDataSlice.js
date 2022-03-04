c33b12f1ecad0878ceef34a73dfa35e1fe52b01c
rk16449 <32778886+rk16449@users.noreply.github.com>
2022-03-04 14:00:06 +0000

    chore: fix wrong password error (#1284)

--- src/features/WalletDataSlice.js
+++ src/features/WalletDataSlice.js
@@ -133,22 +133,22 @@ function setBlockHeightCallBack(item) {
 }
 
 // Load wallet from store
-export const walletLoad = async (name, password) => {
+export const walletLoad = (name, password) => {
 
   wallet = Wallet.load(name, password, testing_mode);
 
   wallet.disableAutoSwaps();
-  await wallet.deRegisterSwaps();
+  wallet.deRegisterSwaps().then(() => {
+    log.info("Wallet "+name+" loaded from memory. ");
   
-  log.info("Wallet "+name+" loaded from memory. ");
-  
-  if (testing_mode) log.info("Testing mode set.");
-  mutex.runExclusive(async () => {
-    await wallet.set_tor_endpoints();
-    wallet.initElectrumClient(setBlockHeightCallBack);
-    wallet.updateSwapStatus();
-    await wallet.updateSwapGroupInfo();
-    wallet.updateSpeedInfo();
+    if (testing_mode) log.info("Testing mode set.");
+    mutex.runExclusive(async () => {
+      await wallet.set_tor_endpoints();
+      wallet.initElectrumClient(setBlockHeightCallBack);
+      wallet.updateSwapStatus();
+      await wallet.updateSwapGroupInfo();
+      wallet.updateSpeedInfo();
+    });
   });
 }
 
