269dd38368521256a22dd9195e8c1fffe7e55c9b
RajKadir <r.kadir@se16.qmul.ac.uk>
2022-03-04 05:42:31 +0000

    fix: delay in deposit with better loading animation

--- src/components/transactionsBTC/transactionsBTC.js
+++ src/components/transactionsBTC/transactionsBTC.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState, useRef} from 'react';
-import {useDispatch, useSelector} from 'react-redux'
-import ReactLoading from 'react-loading';
-import {callDepositInit, callDepositConfirm, setNotification,
+import React, { useEffect, useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  callDepositInit, callDepositConfirm, setNotification,
   callGetUnconfirmedAndUnmindeCoinsFundingTxData, callRemoveCoin,
   callGetConfig,
   callAddDescription,
-  callGetStateCoin,
-  callGetAccount} from '../../features/WalletDataSlice'
-import {fromSatoshi} from '../../wallet'
+  callGetStateCoin
+} from '../../features/WalletDataSlice'
+import { fromSatoshi } from '../../wallet'
 import { CopiedButton } from '../../components'
 import QRCodeGenerator from '../QRCodeGenerator/QRCodeGenerator'
 import CoinDescription from '../inputs/CoinDescription/CoinDescription.js';
@@ -19,6 +19,7 @@ import close_img from "../../images/close-icon.png";
 
 import '../../containers/Deposit/Deposit.css';
 import '../index.css';
+import { Spinner } from 'react-bootstrap';
 
 const keyIcon = (
   <svg
@@ -36,7 +37,9 @@ const keyIcon = (
 const TransactionsBTC = (props) => {
   const [state, setState] = useState({});
   const dispatch = useDispatch();
-  const { depositLoading } = useSelector((state) => state.walletData);
+  const [loading, setLoading] = useState(false);
+  // const { depositLoading } = useSelector((state) => state.walletData); TODO: check why this was placed in redux
+
 
   let testing_mode;
   try {
@@ -44,53 +47,44 @@ const TransactionsBTC = (props) => {
   } catch {
     testing_mode = false;
   }
-  // console.log("TRANSACTION: ",callGetAccount())
+
   // First of all run depositInit for selected deposit amount if not already complete
   props.selectedValues.forEach((item, id) => {
     if (!item.initialised && item.value !== null) {
+      setLoading(true);
       dispatch(callDepositInit(item.value))
-      .then((res => {  // when finished update p_addr in GUI
-        if (res.error===undefined) {
-          props.setValueSelectionAddr(id, res.payload[1]);
-          if(state === {}){
-            setState({}); //update state to refresh TransactionDisplay render
+        .then((res => {  // when finished update p_addr in GUI
+          if (res.error === undefined) {
+            props.setValueSelectionAddr(id, res.payload[1]);
+            let new_deposit_inits = callGetUnconfirmedAndUnmindeCoinsFundingTxData()
+            if (JSON.stringify(deposit_inits) !== JSON.stringify(new_deposit_inits)) {
+              deposit_inits.current = new_deposit_inits
+              setState({}); //update state to refresh TransactionDisplay render
+            }
+            setLoading(false);
           }
-        }
-      }))
+        }))
       props.setValueSelectionInitialised(id, true)
     }
   })
 
   // Fetch all outstanding initialised deposit_inits from wallet
-  let deposit_inits = useRef(callGetUnconfirmedAndUnmindeCoinsFundingTxData()); 
-  
-  // Re-fetch every 10 seconds and update state to refresh render
-  useEffect(() => {
-    const interval = setInterval(() => {
-      let new_deposit_inits = callGetUnconfirmedAndUnmindeCoinsFundingTxData()
-      if (JSON.stringify(deposit_inits)!==JSON.stringify(new_deposit_inits)) {
-        deposit_inits.current = new_deposit_inits
-        setState({}); //update state to refresh TransactionDisplay render
-      }
-    }, 10000);
-    return () => clearInterval(interval);
-  }, []);
+  let deposit_inits = useRef(callGetUnconfirmedAndUnmindeCoinsFundingTxData());
 
   // ** FOR TESTING **
   // Force confirm all outstanding depositInit's.
   // Get all unconfirmed coins and call depositConfirm with dummy txid value.
   const depositConfirm = () => {
     callGetUnconfirmedAndUnmindeCoinsFundingTxData().forEach((statecoin => {
-      dispatch(callDepositConfirm({shared_key_id: statecoin.shared_key_id})).then((res => {
-        if (res.error===undefined) {
-          dispatch(setNotification({msg:"Deposit Complete! StateCoin of "+fromSatoshi(statecoin.value)+" BTC created."}))
+      dispatch(callDepositConfirm({ shared_key_id: statecoin.shared_key_id })).then((res => {
+        if (res.error === undefined) {
+          dispatch(setNotification({ msg: "Deposit Complete! StateCoin of " + fromSatoshi(statecoin.value) + " BTC created." }))
         }
       }));
     }));
   }
 
   const populateWithTransactionDisplayPanels = deposit_inits.current.map((item, index) => {
-
     if (item.value != null) {
       return (
         <div key={index}>
@@ -101,9 +95,9 @@ const TransactionsBTC = (props) => {
               confirmations={item.confirmations}
               address={item.p_addr}
               parent_setState={setState}
-              />
+            />
           </div>
-      </div>
+        </div>
       )
     }
     return null
@@ -111,21 +105,22 @@ const TransactionsBTC = (props) => {
 
   return (
     <div className=" deposit">
-      {depositLoading ? (
+      {loading ? (
         <div className="loading-trans">
           <span>Generating shared key</span>
           <div className="loading-keys">
             <span>{keyIcon}</span>
-            <ReactLoading type={`cylon`} color="#0F54F4" />
+            <Spinner animation='border' variant='primary' size='sm'></Spinner>
+            {/*<ReactLoading type={`cylon`} color="#0F54F4" />*/}
             <span>{keyIcon}</span>
           </div>
         </div>
-      ) : null }
+      ) : null}
       {populateWithTransactionDisplayPanels}
       {testing_mode ?
         <div className="Body">
           <button type="button" className="std-button" onClick={depositConfirm}>
-          PERFORM DEPOSIT CONFIRM
+            PERFORM DEPOSIT CONFIRM
           </button>
         </div>
         :
@@ -139,17 +134,16 @@ const TransactionDisplay = (props) => {
 
   //User added description for coin
   const [description, setDescription] = useState("")
-  const [dscpnConfirm,setDscrpnConfirm] = useState(false)
+  const [dscpnConfirm, setDscrpnConfirm] = useState(false)
 
   const copyAddressToClipboard = () => {
     navigator.clipboard.writeText(props.address);
   }
-
   const getCofirmationsDisplayString = () => {
-    if (props.confirmations===-1) {
+    if (props.confirmations === -1) {
       return "Awaiting.."
     }
-    return props.confirmations+" Confirmations.."
+    return props.confirmations + " Confirmations.."
   }
 
   const onCloseArrowClick = () => {
@@ -160,24 +154,24 @@ const TransactionDisplay = (props) => {
   //This useEffect prevents 
   useEffect(() => {
     let statecoin = callGetStateCoin(props.shared_key_id)
-    if(statecoin.description !== ""){
+    if (statecoin.description !== "") {
       setDscrpnConfirm(true)
       setDescription(statecoin.description)
     }
-  },[props.shared_key_id])
+  }, [props.shared_key_id])
 
   //handle input of coin description
   const handleChange = e => {
     e.preventDefault()
-    if(e.target.value.length < 20){
+    if (e.target.value.length < 20) {
       setDescription(e.target.value)
     }
   }
 
   //Confirm description
   function confirmDescription() {
-    if(dscpnConfirm === false) {
-      callAddDescription(props.shared_key_id,description)
+    if (dscpnConfirm === false) {
+      callAddDescription(props.shared_key_id, description)
     }
     setDscrpnConfirm(!dscpnConfirm)
   }
@@ -186,8 +180,8 @@ const TransactionDisplay = (props) => {
     <div className="Body">
       <div className="deposit-scan">
         {props.confirmations === -1 && (
-          <QRCodeGenerator address = {props.address} amount = {fromSatoshi(props.amount)}
-            level = 'H' />
+          <QRCodeGenerator address={props.address} amount={fromSatoshi(props.amount)}
+            level='H' />
         )}
 
         <div className="deposit-scan-content">
@@ -196,21 +190,19 @@ const TransactionDisplay = (props) => {
               dscrpnConfirm={dscpnConfirm}
               description={description}
               handleChange={handleChange}
-              setDscrpnConfirm={confirmDescription}/>
-
-
+              setDscrpnConfirm={confirmDescription} />
             <div className="deposit-scan-status">
               <span>{getCofirmationsDisplayString()}</span>
-              {props.confirmations === -1 ? <img src={close_img} alt="arrow" onClick={onCloseArrowClick}/> : null}
+              {props.confirmations === -1 ? <img src={close_img} alt="arrow" onClick={onCloseArrowClick} /> : null}
             </div>
           </div>
 
           <div className="deposit-scan-main">
             <div className="deposit-scan-main-item">
-              <img src={btc_img} alt="icon"/>
+              <img src={btc_img} alt="icon" />
               <span><b>{fromSatoshi(props.amount)}</b> BTC</span>
             </div>
-            <img src={arrow_img} alt="arrow"/>
+            <img src={arrow_img} alt="arrow" />
             <div className="deposit-scan-main-item">
               {props.confirmations === -1 ? (
                 <>
@@ -227,8 +219,8 @@ const TransactionDisplay = (props) => {
 
         </div>
       </div>
-      <span className = "deposit-text">Create statecoin by sending {fromSatoshi(props.amount)} BTC to the above address in a SINGLE transaction</span>
-  </div>
+      <span className="deposit-text">Create statecoin by sending {fromSatoshi(props.amount)} BTC to the above address in a SINGLE transaction</span>
+    </div>
   )
 }
 
