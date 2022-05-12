5e1f4ec2544fe68b9e148dfee512d2b7f4760ffa
Lawrence Deacon <lawrence.deacon@gmail.com>
2022-05-12 16:51:08 +0100

    Add file error.ts

---
+++ src/wallet/error.ts
@@ -0,0 +1,29 @@
+import { isNonNullChain } from "typescript"
+
+// Check if returned value from server is an error. Throw if so.
+export const checkForServerError = (response: any) => {
+    if (response == null) {
+        return
+    }
+    let return_val = response?.data
+    if (response.status >= 400) {
+        throw Error(`http status: ${response.status}, data: ${return_val}`)
+    }
+    if (typeof (return_val) == "string" && (return_val.includes("Error") || return_val.includes("error"))) {
+        if (return_val.includes("Not available until")) {
+            throw Error("The server is currently unavailable due to a high request rate. Please try again.")
+        }
+        throw Error(return_val)
+    }
+    if (return_val != null && return_val?.error != null) {
+        throw Error(JSON.stringify(return_val.error))
+    }
+}
+
+export const handlePromiseRejection = (err: any, timeout_msg: string) => {
+    let msg_str = err?.message
+    if (msg_str != null && msg_str.search(/timeout of .*ms exceeded/) > -1) {
+        throw new Error(`${timeout_msg}: ${msg_str}`)
+    } 
+    throw err
+}
