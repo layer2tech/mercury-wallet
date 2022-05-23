072c3876c12e845426a064c286b413848b6084a4
Lawrence Deacon <lawrence.deacon@gmail.com>
2022-05-23 12:37:52 +0100

    Group withdrawal transactions by transaction ID in recovery.

--- src/wallet/recovery.ts
+++ src/wallet/recovery.ts
@@ -13,7 +13,7 @@ import { GET_ROUTE } from '.';
 import {
   transferReceiverFinalizeRecovery, TransferFinalizeDataForRecovery
 } from './mercury/transfer';
-import { setProgressComplete, setProgress} from "../features/WalletDataSlice";
+import { setProgressComplete, setProgress } from "../features/WalletDataSlice";
 import { forEachChild } from "typescript";
 
 
@@ -43,8 +43,8 @@ export const recoverCoins = async (wallet: Wallet, gap_limit: number, dispatch: 
   let percentageComplete
   while (count < gap_limit) {
     for (let i = 0; i < NUM_KEYS_PER_RECOVERY_ATTEMPT; i++) {
-      percentageComplete = Math.floor( wallet.account.chains[0].k /(Math.ceil( gap_limit / NUM_KEYS_PER_RECOVERY_ATTEMPT ) * NUM_KEYS_PER_RECOVERY_ATTEMPT )*100);
-      dispatch(setProgress({msg: percentageComplete}));
+      percentageComplete = Math.floor(wallet.account.chains[0].k / (Math.ceil(gap_limit / NUM_KEYS_PER_RECOVERY_ATTEMPT) * NUM_KEYS_PER_RECOVERY_ATTEMPT) * 100);
+      dispatch(setProgress({ msg: percentageComplete }));
 
       let addr = wallet.account.nextChainAddress(0);
       addrs.push(addr);
@@ -55,7 +55,7 @@ export const recoverCoins = async (wallet: Wallet, gap_limit: number, dispatch: 
     recovery_request = [];
     recovery_data = recovery_data.concat(new_recovery_data_load);
 
-    if(count > gap_limit){
+    if (count > gap_limit) {
       // Once upper limit of addresses have been searched set graphic load complete
       dispatch(setProgressComplete(""))
     }
@@ -118,25 +118,27 @@ export const getFinalizeDataForRecovery = async (wallet: Wallet, wasm: any, reco
 
 // Reconstruct the WithdrawalTxBroadcastInfo for each recoverd statecoin that is withdrawing
 export const groupRecoveredWithdrawalTransactions = (
-    withdrawal_tx_map: Map<string,
+  withdrawal_tx_map: Map<string,
     Set<string>>,
-    withdrawal_addr_map: Map<string, string>,
+  withdraw_tx_id_map: Map<string, Transaction>,
+  withdrawal_addr_map: Map<string, string>,
   statecoins: Map<string, StateCoin>) => {
-  withdrawal_tx_map.forEach((ids, tx_hex) => {
-    const tx = Transaction.fromHex(tx_hex)
+  withdrawal_tx_map.forEach((ids, txid) => {
+    const tx = withdraw_tx_id_map.get(txid)
+    if (tx == null) {
+      return
+    }
     let tx_fee = 0
     const ids_arr = Array.from(ids)
     if (ids_arr.length != tx.ins.length) {
-      console.log(`ids_arr.length: ${ids_arr.length}, tx.ins.length: ${tx.ins.length}`)
       ids.forEach((id) => {
-        console.log(`deleting statecoin id: ${id}`)
         statecoins.delete(id)
       })
       return
-    } 
+    }
     ids.forEach((id) => {
-        const sc = statecoins.get(id)
-        const sc_value = sc != null ? sc.value : 0
+      const sc = statecoins.get(id)
+      const sc_value = sc != null ? sc.value : 0
       tx_fee = tx_fee + sc_value
     })
     tx.outs.forEach((output) => {
@@ -147,10 +149,10 @@ export const groupRecoveredWithdrawalTransactions = (
       return
     }
     ids.forEach((id) => {
-      let statecoin = statecoins.get(id)  
+      let statecoin = statecoins.get(id)
       if (statecoin != null) {
         statecoin.tx_withdraw_broadcast.push(new WithdrawalTxBroadcastInfo(
-          tx_fee, tx, { shared_key_ids: ids_arr }, rec_addr));  
+          tx_fee, tx, { shared_key_ids: ids_arr }, rec_addr));
       }
     })
   })
@@ -158,8 +160,9 @@ export const groupRecoveredWithdrawalTransactions = (
 
 // Gen proof key. Address: tb1qgl76l9gg9qrgv9e9unsxq40dee5gvue0z2uxe2. Proof key: 03b2483ab9bea9843bd9bfb941e8c86c1308e77aa95fccd0e63c2874c0e3ead3f5
 export const addRestoredCoinDataToWallet = async (wallet: Wallet, wasm: any, recoveredCoins: RecoveryDataMsg[]) => {
-  let withdrawal_tx_map = new Map()
-  let withdrawal_addr_map = new Map()
+  let withdrawal_tx_map = new Map<string, Set<string>>()
+  let withdraw_tx_id_map = new Map<string, Transaction>()
+  let withdrawal_addr_map = new Map<string, string>()
   let statecoins = new Map()
   for (let i = 0; i < recoveredCoins.length; i++) {
     let statecoin = null
@@ -185,7 +188,6 @@ export const addRestoredCoinDataToWallet = async (wallet: Wallet, wasm: any, rec
     }
 
     if (statecoin) {
-
       if (recoveredCoins[i].statechain_id) {
         // deposited coin
         let tx_backup = bitcoin.Transaction.fromHex(recoveredCoins[i].tx_hex);
@@ -201,18 +203,21 @@ export const addRestoredCoinDataToWallet = async (wallet: Wallet, wasm: any, rec
         statecoin.tx_hex = recoveredCoins[i].tx_hex;
         statecoin.sc_address = encodeSCEAddress(statecoin.proof_key);
         const withdrawing = recoveredCoins[i].withdrawing
-        if (withdrawing != 'None') {
-          console.log(`recovered coin ${i} withdrawing: ${JSON.stringify(recoveredCoins[i].withdrawing)}`)
+        if (withdrawing != null && withdrawing != 'None') {
           statecoin.setWithdrawing();
           const tx_hex = withdrawing.tx_hex
+          
           const withdraw_transaction = Transaction.fromHex(tx_hex)
+          withdraw_tx_id_map.set(withdraw_transaction.getId(), withdraw_transaction)
 
-          let ids = withdrawal_tx_map.get(tx_hex)
+          let ids = withdrawal_tx_map.get(withdraw_transaction.getId())
+
           if (ids == null) {
             ids = new Set()
-          } 
+          }
+          
           ids.add(statecoin.shared_key_id)
-          withdrawal_tx_map.set(tx_hex, ids)
+          withdrawal_tx_map.set(withdraw_transaction.getId(), ids)
           withdrawal_addr_map.set(withdraw_transaction.getId(), withdrawing.rec_addr)
         } else {
           statecoin.setConfirmed();
@@ -241,7 +246,7 @@ export const addRestoredCoinDataToWallet = async (wallet: Wallet, wasm: any, rec
       statecoins.set(statecoin.shared_key_id, statecoin)
     }
   }
-  groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_addr_map, statecoins)
+  groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdraw_tx_id_map, withdrawal_addr_map, statecoins)
 
   statecoins.forEach((statecoin, _) => {
     wallet.statecoins.addCoin(statecoin);
--- src/wallet/test/recovery.test.js
+++ src/wallet/test/recovery.test.js
@@ -8,6 +8,7 @@ import { addRestoredCoinDataToWallet, groupRecoveredWithdrawalTransactions } fro
 import {
   RECOVERY_DATA, RECOVERY_DATA_C_KEY_CONVERTED, makeTesterStatecoins,
   BTC_ADDRS, recovery_withdrawal_tx_batch, RECOVERY_DATA_WITHDRAWING_BATCH,
+  RECOVERY_DATA_WITHDRAWING_BATCH_2,
   RECOVERY_DATA_WITHDRAWING, RECOVERY_DATA_WITHDRAWING_MIXED
 } from './test_data';
 import {
@@ -97,7 +98,7 @@ describe("Recovery unfinalized", () => {
       wallet.statecoins.coins = [];
       await wallet.genProofKey();
       await wallet.genProofKey();
-      for (let i = 0; i < 50; i++) {
+      for (let i = 0; i < 205; i++) {
         wallet.account.nextChainAddress(0);
       }
     })
@@ -159,6 +160,7 @@ describe("Recovery withdrawing utils", () => {
  
   describe("groupRecoveredWithdrawalTransactions", () => {
     let withdrawal_tx_map
+    let withdrawal_tx_id_map
     let withdrawal_addr_map
     let skids = new Set()
     let statecoins_map = new Map()
@@ -178,15 +180,17 @@ describe("Recovery withdrawing utils", () => {
         statecoins_map.set(statecoin.shared_key_id, statecoin)
       })
       withdrawal_tx_map = new Map()
+      withdrawal_tx_id_map = new Map()
       withdrawal_addr_map = new Map()
     })
     
     test('single withdrawals', () => {
       statecoins.forEach((statecoin, i) => {
-        withdrawal_tx_map.set(transaction_hexs[i], [statecoin.shared_key_id])
+        withdrawal_tx_id_map.set(transactions[i].getId(), transactions[i])
+        withdrawal_tx_map.set(transactions[i].getId(), [statecoin.shared_key_id])
         withdrawal_addr_map.set(transactions[i].getId(), BTC_ADDRS[i])
       })
-      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_addr_map, statecoins_map)
+      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_tx_id_map, withdrawal_addr_map, statecoins_map)
 
       expect(statecoins_map.size).toEqual(2)
       let vals = statecoins_map.values()
@@ -200,18 +204,36 @@ describe("Recovery withdrawing utils", () => {
     })
 
     test('batch withdrawal - wrong number of tx outputs', () => {
-      withdrawal_tx_map.set(transaction_hexs[0], [statecoins[0].shared_key_id, statecoins[1].shared_key_id])
+      withdrawal_tx_id_map.set(transactions[0].getId(), transactions[0])
+      withdrawal_tx_map.set(transactions[0].getId(), [statecoins[0].shared_key_id, statecoins[1].shared_key_id])
       withdrawal_addr_map.set(transactions[0].getId(), BTC_ADDRS[2])
-      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_addr_map, statecoins_map)
+      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_tx_id_map, withdrawal_addr_map, statecoins_map)
       expect(statecoins_map.size).toEqual(0)
     })
 
     test('batch withdrawal', () => {
       console.log(recovery_withdrawal_tx_batch)
       let tx = Transaction.fromHex(recovery_withdrawal_tx_batch)
-      withdrawal_tx_map.set(recovery_withdrawal_tx_batch, [statecoins[0].shared_key_id, statecoins[1].shared_key_id])
+      withdrawal_tx_id_map.set(tx.getId(), tx)
+      withdrawal_tx_map.set(tx.getId(), [statecoins[0].shared_key_id, statecoins[1].shared_key_id])
       withdrawal_addr_map.set(tx.getId(), BTC_ADDRS[2])
-      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_addr_map, statecoins_map)
+      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_tx_id_map, withdrawal_addr_map, statecoins_map)
+      expect(statecoins_map.size).toEqual(2)
+      let vals = statecoins_map.values()
+      //for (let i = 0; i < statecoins_map.size; i++) {
+      let broadcast_infos = vals.next().value.tx_withdraw_broadcast
+      expect(broadcast_infos.length).toEqual(1)
+      expect(broadcast_infos[0].rec_addr).toEqual(BTC_ADDRS[2])
+    })
+
+    test('batch withdrawal 2', () => {
+      const tx_hex = RECOVERY_DATA_WITHDRAWING_BATCH_2[0].withdrawing.tx_hex
+      console.log(tx_hex)
+      let tx = Transaction.fromHex(tx_hex)
+      withdrawal_tx_id_map.set(tx.getId(), tx)
+      withdrawal_tx_map.set(tx.getId(), [statecoins[0].shared_key_id, statecoins[1].shared_key_id])
+      withdrawal_addr_map.set(tx.getId(), BTC_ADDRS[2])
+      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_tx_id_map, withdrawal_addr_map, statecoins_map)
       expect(statecoins_map.size).toEqual(2)
       let vals = statecoins_map.values()
       //for (let i = 0; i < statecoins_map.size; i++) {
@@ -224,14 +246,16 @@ describe("Recovery withdrawing utils", () => {
     test('single/batch withdrawal', () => {
       console.log(recovery_withdrawal_tx_batch)
       let tx = Transaction.fromHex(recovery_withdrawal_tx_batch)
-      withdrawal_tx_map.set(recovery_withdrawal_tx_batch, [RECOVERY_DATA_WITHDRAWING_BATCH[0].shared_key_id, RECOVERY_DATA_WITHDRAWING_BATCH[1].shared_key_id])
+      withdrawal_tx_id_map.set(tx.getId(), tx)
+      withdrawal_tx_map.set(tx.getId(), [RECOVERY_DATA_WITHDRAWING_BATCH[0].shared_key_id, RECOVERY_DATA_WITHDRAWING_BATCH[1].shared_key_id])
       withdrawal_addr_map.set(tx.getId(), BTC_ADDRS[2])
       statecoins.forEach((statecoin, i) => {
-        withdrawal_tx_map.set(transaction_hexs[i], [statecoin.shared_key_id])
+        withdrawal_tx_id_map.set(transactions[i].getId(), transactions[i])
+        withdrawal_tx_map.set(transactions[i].getId(), [statecoin.shared_key_id])
         withdrawal_addr_map.set(transactions[i].getId(), BTC_ADDRS[i])
       })
 
-      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_addr_map, statecoins_map)
+      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_tx_id_map, withdrawal_addr_map, statecoins_map)
       expect(statecoins_map.size).toEqual(2)
       let vals = statecoins_map.values()
       //for (let i = 0; i < statecoins_map.size; i++) {
@@ -239,6 +263,29 @@ describe("Recovery withdrawing utils", () => {
       expect(broadcast_infos.length).toEqual(1)
       expect(broadcast_infos[0].rec_addr).toEqual("tb1q6xwt00hnwcrtlunvnz8u0xrtdxv5ztx7pj44cp")
     })
+
+    test('single/batch withdrawal 2', () => {
+      console.log(recovery_withdrawal_tx_batch)
+      let tx = Transaction.fromHex(recovery_withdrawal_tx_batch)
+      withdrawal_tx_id_map.set(tx.getId(), tx)
+      withdrawal_tx_map.set(tx.getId(), [RECOVERY_DATA_WITHDRAWING_BATCH_2[0].shared_key_id, RECOVERY_DATA_WITHDRAWING_BATCH_2[1].shared_key_id])
+      withdrawal_addr_map.set(tx.getId(), BTC_ADDRS[2])
+      statecoins.forEach((statecoin, i) => {
+        withdrawal_tx_id_map.set(transactions[i].getId(), transactions[i])
+        withdrawal_tx_map.set(transactions[i].getId(), [statecoin.shared_key_id])
+        withdrawal_addr_map.set(transactions[i].getId(), BTC_ADDRS[i])
+      })
+
+      groupRecoveredWithdrawalTransactions(withdrawal_tx_map, withdrawal_tx_id_map, withdrawal_addr_map, statecoins_map)
+      expect(statecoins_map.size).toEqual(2)
+      let vals = statecoins_map.values()
+      //for (let i = 0; i < statecoins_map.size; i++) {
+      let broadcast_infos = vals.next().value.tx_withdraw_broadcast
+      expect(broadcast_infos.length).toEqual(1)
+      expect(broadcast_infos[0].rec_addr).toEqual("tb1q6xwt00hnwcrtlunvnz8u0xrtdxv5ztx7pj44cp")
+    })
+   
+
   })
 })
 
@@ -490,5 +537,100 @@ describe("Recovery withdrawing mixed", () => {
         RECOVERY_DATA_WITHDRAWING_MIXED[3].shared_key_id]
     })
   });
+})
+
+
+describe("Recovery withdrawing batch 2", () => {
+  const MNEMONIC = "jar coyote eyebrow tell world curious breeze text beef lawn then clump"
+  // client side's mock
+  let wasm_mock = jest.genMockFromModule('../mocks/mock_wasm');
+  // server side's mock
+  let http_mock = jest.genMockFromModule('../mocks/mock_http_client');
+  let wallet
+  beforeAll(async () => {
+    wallet = await Wallet.buildMock(bitcoin.networks.bitcoin, http_mock, wasm_mock, MNEMONIC);
+    wallet.statecoins.coins = [];
+    //await wallet.genProofKey();
+    
+    for (let i = 0; i < 5; i++) {
+      //wallet.account.nextChainAddress(0);
+      await wallet.genProofKey();
+    }
+  })
+
+
+  test('withdrawal tx compare', () => {
+    const tx0 = Transaction.fromHex(RECOVERY_DATA_WITHDRAWING_BATCH_2[0].withdrawing.tx_hex)
+    const tx1 = Transaction.fromHex(RECOVERY_DATA_WITHDRAWING_BATCH_2[1].withdrawing.tx_hex)
+    expect(tx0.getId()).toEqual(tx1.getId())
+    expect(tx0.getHash()).toEqual(tx1.getHash())
+  })
+
+  test('run recovery withdrawing batch 2', async () => {
+
+    http_mock.post = jest.fn().mockReset()
+      .mockReturnValueOnce(RECOVERY_DATA_WITHDRAWING_BATCH_2)
+      .mockReturnValue([]);
+    // Create mock HTTP client for
+
+    wasm_mock.convert_bigint_to_client_curv_version = jest.fn(() => RECOVERY_DATA_C_KEY_CONVERTED);
+    // Mock wasm client
+
+    expect(wallet.statecoins.coins.length).toBe(0);
+    // Check no statecoins in Mock wallet
+
+    let wallet_json = wallet.toEncryptedJSON()
+    // Get encrypted JSON file
+
+    let store = configureStore({ reducer: reducers, });
+    // Load state
+
+    render(store,
+      <TestComponent
+        wallet_json={wallet_json}
+        fn={addRestoredCoinDataToWallet}
+        args={[wallet, wasm_mock, RECOVERY_DATA_WITHDRAWING_BATCH_2]}
+      />
+    )
+    // Test component rendered so wallet loaded to state when function called
+
+
+    fireEvent(screen.getByText(/FireFunction/i), new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    }))
+    // Firing this event, fires the function
+
+    expect(wallet.statecoins.coins.length).toBe(2);
+    expect(wallet.statecoins.coins[0].status).toBe(STATECOIN_STATUS.WITHDRAWING);
+
+    expect(wallet.statecoins.coins[0].value).toBe(RECOVERY_DATA_WITHDRAWING_BATCH_2[0].amount);
+    expect(wallet.statecoins.coins[0].sc_address).toBe(encodeSCEAddress(RECOVERY_DATA_WITHDRAWING_BATCH_2[0].proof_key));
+    expect(wallet.statecoins.coins[0].tx_withdraw_broadcast.length).toBe(1)
+    expect(wallet.statecoins.coins[0].tx_withdraw_broadcast[0].rec_addr).toEqual(RECOVERY_DATA_WITHDRAWING_BATCH_2[0].withdrawing.rec_addr)
+    expect(wallet.statecoins.coins[0].tx_withdraw_broadcast[0].tx_fee).toEqual(515)
+    expect(wallet.statecoins.coins[0].tx_withdraw_broadcast[0].tx.getId()).toEqual(Transaction.fromHex(RECOVERY_DATA_WITHDRAWING_BATCH_2[0].withdrawing.tx_hex).getId())
+    expect(wallet.statecoins.coins[0].tx_withdraw_broadcast[0].txid).toEqual(Transaction.fromHex(RECOVERY_DATA_WITHDRAWING_BATCH_2[0].withdrawing.tx_hex).getId())
+    expect(wallet.statecoins.coins[0].tx_withdraw_broadcast[0].withdraw_msg_2).toEqual({
+      shared_key_ids:
+        [RECOVERY_DATA_WITHDRAWING_BATCH_2[0].shared_key_id,
+        RECOVERY_DATA_WITHDRAWING_BATCH_2[1].shared_key_id]
+    })
+
+    expect(wallet.statecoins.coins[1].status).toBe(STATECOIN_STATUS.WITHDRAWING);
+    expect(wallet.statecoins.coins[1].value).toBe(RECOVERY_DATA_WITHDRAWING_BATCH_2[1].amount);
+    expect(wallet.statecoins.coins[1].sc_address).toBe(encodeSCEAddress(RECOVERY_DATA_WITHDRAWING_BATCH_2[1].proof_key));
+    expect(wallet.statecoins.coins[1].tx_withdraw_broadcast[0].rec_addr).toEqual(RECOVERY_DATA_WITHDRAWING_BATCH_2[1].withdrawing.rec_addr)
+    expect(wallet.statecoins.coins[1].tx_withdraw_broadcast[0].tx_fee).toEqual(515)
+    expect(wallet.statecoins.coins[1].tx_withdraw_broadcast[0].tx.getId()).toEqual(Transaction.fromHex(RECOVERY_DATA_WITHDRAWING_BATCH_2[1].withdrawing.tx_hex).getId())
+    expect(wallet.statecoins.coins[1].tx_withdraw_broadcast[0].txid).toEqual(Transaction.fromHex(RECOVERY_DATA_WITHDRAWING_BATCH_2[1].withdrawing.tx_hex).getId())
+    expect(wallet.statecoins.coins[1].tx_withdraw_broadcast[0].withdraw_msg_2).toEqual({
+      shared_key_ids:
+        [RECOVERY_DATA_WITHDRAWING_BATCH_2[0].shared_key_id,
+        RECOVERY_DATA_WITHDRAWING_BATCH_2[1].shared_key_id]
+    })
+  });
+
+
 })
 
