b51ff0723e5585f8c16e5b4314ac01774cbd7272
RajKadir <r.kadir@se16.qmul.ac.uk>
2022-07-07 17:45:11 +0100

    debug: add a main test to debug tests further

---
+++ src/wallet/test/main.test.js
@@ -0,0 +1,16 @@
+const wtf = require("wtfnode");
+const whyRunNodelog = require("why-is-node-running"); // should be your first require
+
+// ... rest of your require statements and such ....
+
+afterAll(async () => {
+  wtf.dump();
+  whyRunNodelog();
+  console.log("process._getActiveHandles()", process._getActiveHandles());
+});
+
+describe("main test", () => {
+  test("main test", () => {
+    expect(1).toBe(1);
+  });
+});
