644cd84ffe247e25dc6d1d278f3b436892aedf11
RajKadir <r.kadir@se16.qmul.ac.uk>
2022-07-08 17:27:40 +0100

    chore: add base packages to run webpack 5

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-'use strict';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
-import { getDefaultMiddleware } from '@reduxjs/toolkit';
+"use strict";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { getDefaultMiddleware } from "@reduxjs/toolkit";
 
-import App from './containers/App/App';
-import rootReducer from './reducers'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import App from "./containers/App/App";
+import rootReducer from "./reducers";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 const SuppressWarnings = require("suppress-warnings");
 
 SuppressWarnings([
-  (warning, name, ctor) => name === "DeprecationWarning" && warning.toString() ===
-    "TransactionBuilder will be removed in the future. (v6.x.x or later) Please use the Psbt class instead.Examples of usage are available in the transactions - psbt.js integration test file on our Github.A high level explanation is available in the psbt.ts and psbt.js files as well."
+  (warning, name, ctor) =>
+    name === "DeprecationWarning" &&
+    warning.toString() ===
+      "TransactionBuilder will be removed in the future. (v6.x.x or later) Please use the Psbt class instead.Examples of usage are available in the transactions - psbt.js integration test file on our Github.A high level explanation is available in the psbt.ts and psbt.js files as well.",
 ]);
 
 // Store is the global wallet GUI data structure.
@@ -23,16 +25,13 @@ SuppressWarnings([
 const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware({
-  serializableCheck: false
-}),
-})
-
-
+    serializableCheck: false,
+  }),
+});
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
