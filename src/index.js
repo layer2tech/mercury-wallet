e6c80d0395311b83f1be997bc3a2218b9cbe4aae
RajKadir <r.kadir@se16.qmul.ac.uk>
2022-10-12 18:13:26 +0100

    chore: fix order of app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 "use strict";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./containers/App/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { store, Persistor } from "./application/reduxStore";
 import { PersistGate } from "redux-persist/integration/react";
+
+import App from "./containers/App/App";
 
 ReactDOM.render(
   <Provider store={store}>
