45da68bccbbebb3633ba2f124c0035b2b7e3fc09
RajKadir <r.kadir@se16.qmul.ac.uk>
2022-10-13 16:59:44 +0100

    chore: fix webStore to load statecoins

--- src/application/webStore.js
+++ src/application/webStore.js
@@ -22,62 +22,81 @@ export class WebStore {
     let walletVar = undefined;
     let walletObject = undefined;
     let walletAttribute = undefined;
+    let walletAttributeProperty = undefined;
     let walletInfo = store.getState().walletInfo;
     let wallets = walletInfo.wallets;
     let loginInfo = walletInfo.loginInfo;
-    //console.log("WalletInfo ->", walletInfo);
-    //console.log("LoginInfo ->", loginInfo);
-    //console.log("Value ->", value);
+    console.log("WalletInfo ->", walletInfo);
+    console.log("LoginInfo ->", loginInfo);
+    console.log("Value ->", value);
 
     if (value !== undefined && value.includes(".")) {
       walletVar = value.split("."); // array of for e.g. wallet.name into ["wallet", "name"]
       walletObject = walletVar[0] + ""; // key to whole object {}
       walletAttribute = walletVar[1] + ""; // variable {}.variable
+      walletAttributeProperty = walletVar[2] + "";
+
       // wallets : { test123:{}, wallet2:{}, wallet3: {}}
       // wallets.walletName.walletAttribute
       let getWalletAttribute = wallets[walletObject][walletAttribute];
+
+      if (walletAttribute === "name") {
+        console.log("return the name:", wallets[walletObject]);
+
+        if (wallets[walletObject] !== undefined) {
+          return walletObject + "";
+        }
+      }
+
       if (walletAttribute === "statecoins") {
+        if (walletAttributeProperty === "coins") {
+          return wallets[walletObject].statecoins.coins;
+        } else if (walletAttributeProperty === "swapped_coins") {
+          return wallets[walletObject].statecoins.swapped_coins;
+        }
         return wallets[walletObject].statecoins;
       } else if (walletAttribute === "statecoins_obj") {
-        //console.log("get->statecoins_obj", walletAttribute);
-        //console.log("the value was:", value);
-        //console.log(wallets[walletObject].statecoins_obj);
+        if (walletAttributeProperty === "coins") {
+          return wallets[walletObject].statecoins_obj.coins;
+        } else if (walletAttributeProperty === "swapped_coins") {
+          return wallets[walletObject].statecoins_obj.swapped_coins;
+        }
         return wallets[walletObject].statecoins_obj;
       } else if (getWalletAttribute === {}) {
         return undefined;
       } else if (walletAttribute === "swapped_statecoins_obj") {
-        //console.log("get->swapped_coins_obj...");
+        console.log("get->swapped_coins_obj...");
         return wallets[walletObject].swapped_statecoins_obj;
       } else {
         return getWalletAttribute;
       }
     } else {
       if (value === undefined) {
-        //console.log("value !=== undefined?");
+        console.log("value !=== undefined?");
         const hasKeys = !!Object.keys(wallets).length;
         if (!hasKeys) {
-          //console.log("there is no keys here...");
+          console.log("there is no keys here...");
           return null;
         } else {
-          //console.log("return the wallets object");
+          console.log("return the wallets object");
           return wallets;
         }
       }
       // get from login store
       else if (value.includes("logins.")) {
-        //console.log("Logins...");
+        console.log("Logins...");
         return loginInfo[value];
       } else if (wallets[value] !== undefined) {
-        //console.log("wallets[value] !== undefined");
+        console.log("wallets[value] !== undefined");
         return wallets[value];
       } else {
-        //console.log(wallets["12345"].name);
-        //console.log("wallets is equal to:", wallets);
-        //console.log("looking for value ", value);
+        console.log(wallets["12345"].name);
+        console.log("wallets is equal to:", wallets);
+        console.log("looking for value ", value);
       }
     }
 
-    //console.log("found nothing.. returning false...");
+    console.log("found nothing.. returning false...");
     return undefined;
   }
 
