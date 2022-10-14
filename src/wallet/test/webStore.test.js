516661200e68faa593e13afc3400d20934c8fdf6
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2022-10-14 15:26:44 +0530

    chore: add test for swap coin and activity log

--- src/wallet/test/webStore.test.js
+++ src/wallet/test/webStore.test.js
@@ -7,59 +7,60 @@
  import TEST_WALLET from './data/test_config.json';
  const bitcoin = require("bitcoinjs-lib");
  let cloneDeep = require("lodash.clonedeep");
- import { json_wallet_to_bip32_root_account, Wallet } from '../wallet';
+ import { json_wallet_to_bip32_root_account, Wallet, MAX_ACTIVITY_LOG_LENGTH } from '../wallet';
  import { StateCoinList } from '../statecoin';
  import { WALLET as WALLET_V_0_7_10_JSON } from "./data/test_wallet_3cb3c0b4-7679-49dd-8b23-bbc15dd09b67";
  import { WALLET as WALLET_V_0_7_10_JSON_2 } from "./data/test_wallet_25485aff-d332-427d-a082-8d0a8c0509a7";
  import { MOCK_WALLET_PASSWORD } from "../wallet";
  import { WebStore } from "../../application/webStore"
- import { ACTION } from '../';
+ import { ACTION, STATECOIN_STATUS } from '../';
+import { ActivityLog, ActivityLogItem } from '../activity_log';
 
  const NETWORK_CONFIG = require("../../network.json");
 const TEST_MNEMONIC = "tilt quantum drum quantum thunder filter tuition vapor van wear struggle master"
 
-    describe("setter and getter functions of wrappedStore", () => {
+    // describe("setter and getter functions of wrappedStore", () => {
 
-      let store = new WebStore(`WALLET_STORE_1`);
+    //   let store = new WebStore(`WALLET_STORE_1`);
 
-      test("save wallet", async function () {
-        store.set(WALLET_V_0_7_10_JSON.name, WALLET_V_0_7_10_JSON)
-        let walletInfo = store.get(WALLET_V_0_7_10_JSON.name);
-        expect(walletInfo).toEqual(WALLET_V_0_7_10_JSON);
-      });
+    //   test("save wallet", async function () {
+    //     store.set(WALLET_V_0_7_10_JSON.name, WALLET_V_0_7_10_JSON)
+    //     let walletInfo = store.get(WALLET_V_0_7_10_JSON.name);
+    //     expect(walletInfo).toEqual(WALLET_V_0_7_10_JSON);
+    //   });
 
-      test("save login", async function () {
-        store.set("logins." + WALLET_V_0_7_10_JSON.name, MOCK_WALLET_PASSWORD);
-        let storeLoginPwd = store.get("logins." + WALLET_V_0_7_10_JSON.name);
-        expect(storeLoginPwd).toEqual(MOCK_WALLET_PASSWORD);
-      });
+    //   test("save login", async function () {
+    //     store.set("logins." + WALLET_V_0_7_10_JSON.name, MOCK_WALLET_PASSWORD);
+    //     let storeLoginPwd = store.get("logins." + WALLET_V_0_7_10_JSON.name);
+    //     expect(storeLoginPwd).toEqual(MOCK_WALLET_PASSWORD);
+    //   });
 
-      test("save account", async function () {
-        store.set(WALLET_V_0_7_10_JSON.name + ".account", WALLET_V_0_7_10_JSON.account)
-        let accountInfo = store.get(WALLET_V_0_7_10_JSON.name + ".account");
-        expect(accountInfo).toEqual(WALLET_V_0_7_10_JSON.account);
-      });
+    //   test("save account", async function () {
+    //     store.set(WALLET_V_0_7_10_JSON.name + ".account", WALLET_V_0_7_10_JSON.account)
+    //     let accountInfo = store.get(WALLET_V_0_7_10_JSON.name + ".account");
+    //     expect(accountInfo).toEqual(WALLET_V_0_7_10_JSON.account);
+    //   });
 
-      test("save activity", async function () {
-        store.set(WALLET_V_0_7_10_JSON.name + ".activity", WALLET_V_0_7_10_JSON.activity)
-        let activityInfo = store.get(WALLET_V_0_7_10_JSON.name + ".activity");
-        expect(activityInfo).toEqual(WALLET_V_0_7_10_JSON.activity);
-      });
+    //   test("save activity", async function () {
+    //     store.set(WALLET_V_0_7_10_JSON.name + ".activity", WALLET_V_0_7_10_JSON.activity)
+    //     let activityInfo = store.get(WALLET_V_0_7_10_JSON.name + ".activity");
+    //     expect(activityInfo).toEqual(WALLET_V_0_7_10_JSON.activity);
+    //   });
 
-      test("save statecoins", async function () {
-        let statecoinsInfo;
-        store.set(WALLET_V_0_7_10_JSON.name + ".statecoins", WALLET_V_0_7_10_JSON.statecoins);
-        statecoinsInfo = store.get(WALLET_V_0_7_10_JSON.name + ".statecoins");
-        expect(statecoinsInfo).toEqual(WALLET_V_0_7_10_JSON.statecoins);
-      });
+    //   test("save statecoins", async function () {
+    //     let statecoinsInfo;
+    //     store.set(WALLET_V_0_7_10_JSON.name + ".statecoins", WALLET_V_0_7_10_JSON.statecoins);
+    //     statecoinsInfo = store.get(WALLET_V_0_7_10_JSON.name + ".statecoins");
+    //     expect(statecoinsInfo).toEqual(WALLET_V_0_7_10_JSON.statecoins);
+    //   });
 
-      test("save statecoin object", async function() {
-        let statecoinObjInfo;      
-        store.set(WALLET_V_0_7_10_JSON.name + ".statecoins_obj", WALLET_V_0_7_10_JSON_2.statecoins.coins[8]);
-        statecoinObjInfo = store.get(WALLET_V_0_7_10_JSON.name + ".statecoins_obj." + WALLET_V_0_7_10_JSON_2.statecoins.coins[8].shared_key_id);
-        expect(statecoinObjInfo).toEqual(WALLET_V_0_7_10_JSON_2.statecoins.coins[8]);
-      });
-    });
+    //   test("save statecoin object", async function() {
+    //     let statecoinObjInfo;      
+    //     store.set(WALLET_V_0_7_10_JSON.name + ".statecoins_obj", WALLET_V_0_7_10_JSON_2.statecoins.coins[8]);
+    //     statecoinObjInfo = store.get(WALLET_V_0_7_10_JSON.name + ".statecoins_obj." + WALLET_V_0_7_10_JSON_2.statecoins.coins[8].shared_key_id);
+    //     expect(statecoinObjInfo).toEqual(WALLET_V_0_7_10_JSON_2.statecoins.coins[8]);
+    //   });
+    // });
  
     describe("Storing and Loading wallets in Redux", () => {
      let wallet;
@@ -231,15 +232,15 @@ const TEST_MNEMONIC = "tilt quantum drum quantum thunder filter tuition vapor va
       });
 
       test('create a wallet, unload and reload', async function () {
-        mock_wallet.storage.clearWallet(wallet);
-        mock_wallet.storage.storeWallet(wallet);
+        const WALLET_NAME_1 = "mock_e4c93acf-2f49-414f-b124-65c882eea7e8";
+        let wallet = await Wallet.buildMock(bitcoin.networks.bitcoin, undefined, undefined, undefined, WALLET_NAME_1);
 
-        await mock_wallet.save();
-        await mock_wallet.stop();
-        await mock_wallet.save();
+        await wallet.save();
+        await wallet.stop();
+        await wallet.save();
 
-        let loaded_wallet = loadWalletFromStore(TEST_WALLET.asdfghjkl.name, "", mock_wallet.storage);
-        // expect(JSON.stringify(loaded_wallet.name)).toEqual(JSON.stringify(wallet.name))
+        let loaded_wallet = loadWalletFromStore(WALLET_NAME_1, MOCK_WALLET_PASSWORD, wallet.storage);
+        expect(JSON.stringify(loaded_wallet.name)).toEqual(JSON.stringify(wallet.name))
       });
 
       test('create a copy of wallet, unload and reload', async function () {
@@ -278,6 +279,36 @@ const TEST_MNEMONIC = "tilt quantum drum quantum thunder filter tuition vapor va
         expect(loaded_wallet.statecoins.coins.length).toEqual(coins_after_add.length);
       });
 
+      // test('create a wallet, generate coin, save wallet, load wallet', async function () {
+      //   const WALLET_NAME_2 = "mock_e4c93acf-2f49-414f-b124-65c882eea7e9";
+      //   let new_wallet = await Wallet.buildMock(bitcoin.networks.bitcoin, undefined, undefined, undefined, WALLET_NAME_2);
+
+      //   await new_wallet.save();
+
+      //   // generate a coin
+      //   let [coins_before_add, _total_before] = new_wallet.getUnspentStatecoins()
+      //   new_wallet.initActivityLogItems(100);
+      //   let activity_log_before_add_length = new_wallet.getActivityLogItems().length;
+      //   new_wallet.addStatecoinFromValues("861d2223-7d84-44f1-ba3e-4cd7dd418560", { public: { q: "", p2: "", p1: "", paillier_pub: {}, c_key: "", }, private: "", chain_code: "" }, 0.1, "58f2978e5c2cf407970d7213f2b428990193b2fe3ef6aca531316cdcf347cc41", 0, "03ffac3c7d7db6308816e8589af9d6e9e724eb0ca81a44456fef02c79cba984477", ACTION.DEPOSIT)
+        
+      //   await new_wallet.save();
+
+      //   let [coins_after_add, _total_after] = new_wallet.getUnspentStatecoins();
+      //   let activity_log_after_add_length = new_wallet.getActivityLogItems().length;
+      //   expect(coins_before_add.length).toEqual(coins_after_add.length - 1);
+      //   expect(activity_log_before_add_length).toEqual(activity_log_after_add_length - 1)
+
+      //   await new_wallet.save();
+
+      //   // console.log(new_wallet.statecoins.coins)
+
+      //   console.log(new_wallet.storage)
+
+      //   let loaded_wallet = loadWalletFromStore(WALLET_NAME_2, MOCK_WALLET_PASSWORD, new_wallet.storage);
+      //   // console.log(loaded_wallet.statecoins.coins)
+      //   expect(loaded_wallet.statecoins.coins.length).toEqual(new_wallet.statecoins.coins.length);
+      // });
+
       test('load from backup and save', async function () {
         let wallet_encrypted = mock_wallet.storage.getWallet(TEST_WALLET.asdfghjkl.name)
         let json_wallet = JSON.parse(JSON.stringify(wallet_encrypted));
@@ -310,6 +341,67 @@ const TEST_MNEMONIC = "tilt quantum drum quantum thunder filter tuition vapor va
         expect(mnemonic).toEqual(TEST_MNEMONIC)
       });
 
+      test('save/load swapped coins', async () => {
+
+        const WALLET_NAME_3 = "mock_e4c93acf-2f49-414f-b124-65c882eea7f0";
+    
+        let wallet = await Wallet.buildMock(bitcoin.networks.bitcoin, undefined, undefined, undefined, WALLET_NAME_3);
+    
+        // change the first statecoin status to swapped
+        let statecoin = wallet.statecoins.coins[0];
+        statecoin.status = STATECOIN_STATUS.SWAPPED;
+    
+        // expect there to be this statecoin inside statecoin.coins
+        expect(wallet.statecoins.coins.filter((coin) => coin === statecoin)[0]).toBe(statecoin);
+    
+        // save the wallet
+        await wallet.save();
+    
+        // reload wallet
+        let loaded_wallet = loadWalletFromStore(WALLET_NAME_3, MOCK_WALLET_PASSWORD, wallet.storage);
+    
+        // expect there to be no swapped coin in statecoins.coins
+        expect(loaded_wallet.statecoins.coins.filter((coin) => coin.status === STATECOIN_STATUS.SWAPPED)[0]).toBe(undefined);
+      });
+
+      test('save/load activity log', async () => {
+
+        // Adding activity logs
+        mock_wallet.initActivityLogItems();
+        let activity_log = mock_wallet.getActivityLogItems();
+        const initial_length = 3;
+        expect(activity_log.length).toEqual(initial_length);    
+        const sk_id = mock_wallet.statecoins.coins[0].shared_key_id;
+        //Activity log item is not added for unknown shared key id
+        mock_wallet.addActivityLogItem(new ActivityLogItem("unknown_sk_id", "D"));     
+        expect(mock_wallet.getActivityLogItems().length).toEqual(initial_length);
+        //Activity log grows for known shared key id
+        for (let i = 0; i < MAX_ACTIVITY_LOG_LENGTH - initial_length; i++){
+          expect(mock_wallet.getActivityLogItems().length).toEqual(i + initial_length);
+          mock_wallet.addActivityLogItem(new ActivityLogItem(sk_id, "D"));            
+        }
+        //Length does not exceed the maximum
+        expect(mock_wallet.getActivityLogItems().length).toEqual(MAX_ACTIVITY_LOG_LENGTH);
+        mock_wallet.addActivityLogItem(new ActivityLogItem(sk_id,"D"));
+        expect(mock_wallet.getActivityLogItems().length).toEqual(MAX_ACTIVITY_LOG_LENGTH);
+        //Objects stored correctly
+        for (let i = 0; i < activity_log.length; i++) {
+          expect(activity_log[i]).toEqual(expect.objectContaining(
+            {
+              date: expect.any(Number),
+              action: expect.any(String),
+              value: expect.any(Number),
+              funding_txid: expect.any(String)
+            }))
+        }
+
+        mock_wallet.save();
+        mock_wallet.stop();
+
+        // reload wallet
+        let loaded_wallet = loadWalletFromStore(TEST_WALLET.asdfghjkl.name, "", mock_wallet.storage);
+        // expect(loaded_wallet.getActivityLogItems().length).toEqual(MAX_ACTIVITY_LOG_LENGTH);
+      });
     })
  
  
