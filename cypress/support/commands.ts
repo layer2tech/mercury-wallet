2515297a92c91c25c375568f285d67813b46c1ce
RajKadir <r.kadir@se16.qmul.ac.uk>
2022-11-02 19:43:58 +0000

    chore: cypress ignore mercurywallet internal exceptions

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -12,6 +12,12 @@ function arrayDiff(a, b) {
   });
 }
 
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+
 Cypress.Commands.add("login", () => {
   cy.visit("/");
   cy.get("[data-cy=create-wallet-btn]").click();
