34387e688cfdfe3b9c3e22af2ffce59e06520788
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2022-11-03 20:33:41 +0530

    chore: fix failure due to network error

--- src/wallet/http_client.ts
+++ src/wallet/http_client.ts
@@ -60,7 +60,7 @@ Object.freeze(POST_ROUTE);
 
 export const TOR_URL = "http://localhost:3001";
 
-export const I2P_URL = "http://localhost:3002";
+export const I2P_URL = "http://localhost:3001";
 
 export class HttpClient {
   endpoint: string
