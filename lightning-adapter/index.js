4acf81fc1e3e7c202d5c1a002a85a48f8ba5e4bd
Sam Gershuny <samgershuny@gmail.com>
2022-11-18 19:20:36 +0200

    lightning

--- lightning-adapter/index.js
+++ lightning-adapter/index.js
@@ -1,8 +1,20 @@
 import * as ldk from "lightningdevkit";
 import * as fs from "fs";
+// import YourFeeEstimator from './lib/YourFeeEstimator.ts';
 import { strict as assert } from "assert";
+import YourFeeEstimator from "./lib/YourFeeEstimator";
+
+
+
 const wasm_file = fs.readFileSync(
   "node_modules/lightningdevkit/liblightningjs.wasm"
 );
+
 await ldk.initializeWasmFromBinary(wasm_file);
+
+const fee_estimator = ldk.FeeEstimator.new_impl(new YourFeeEstimator());
+
+console.log(fee_estimator);
+
+
 
