a84c74b20d0368214ca1e019d13c287498f87d40
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-01-11 18:23:51 +0000

    chore: cleanup and move routes to separate files

---
+++ lightning-adapter/getRoutes.js
@@ -0,0 +1,33 @@
+const LDK = require("./index.js");
+const express = require("express");
+const router = express.Router();
+
+router.get("/peerlist", async function (req, res) {
+  // sample public list
+  let data = [
+    {
+      node: "WalletOfSatoshi.com",
+      host: "170.75.163.209",
+      port: "9735",
+      pubkey:
+        "035e4ff418fc8b5554c5d9eea66396c227bd429a3251c8cbc711002ba215bfc226",
+    },
+    {
+      node: "ACINQ",
+      host: "3.33.236.230",
+      port: "9735",
+      pubkey:
+        "03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f",
+    },
+    {
+      node: "CoinGate",
+      host: "3.124.63.44",
+      port: "9735",
+      pubkey:
+        "0242a4ae0c5bef18048fbecf995094b74bfb0f7391418d71ed394784373f41e4f3",
+    },
+  ];
+  res.status(200).json(data);
+});
+
+module.exports = router;
