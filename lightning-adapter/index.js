d665832cc84635dc9453509f3c1465b2052a346b
rk16449 <32778886+rk16449@users.noreply.github.com>
2023-01-12 15:45:31 +0000

    Feat/ldk routes (#1872)
    * chore: cleanup and move routes to separate files
    
    * chore: cleanup routes
    
    * chore: add cleaned up index.js
    
    * chore: add db changes

--- lightning-adapter/index.js
+++ lightning-adapter/index.js
@@ -1,88 +1,50 @@
 "use strict";
 
+const { importLDK, LDKClient } = require("./lightningClient");
 const express = require("express");
-var cors = require("cors");
-var path = require("path");
+const cors = require("cors");
+const path = require("path");
 const fs = require("fs");
+const bodyParser = require("body-parser");
+const LightningClient = require("./lightning.js");
 
-// const { logger, log } = require("./logger");
-
-// const logDataDir = process.argv[4];
-
+// Constants
 const PORT = 3003;
-var bodyParser = require("body-parser");
-// const dataDir = path.join(logDataDir, "ldk")
 
-/**
- * STARTING SERVER ON PORT
- */
+// Routers
+const getRoutes = require("./routes/getRoutes");
+const postRoutes = require("./routes/postRoutes");
 
+// Express app
 const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
-async function importLDK(){
-  // const ldk = await import("lightningdevkit");
-  // console.log("ldk: ", ldk);
-  // console.log("initialize the wasm from fetch...");
-  // const wasm_file = fs.readFileSync("node_modules/lightningdevkit/liblightningjs.wasm")
-  // await ldk.initializeWasmFromBinary(wasm_file);
-  // await ldk.initializeWasmWebFetch("liblightningjs.wasm");
-  var LightningClient = (await import('./lightning.mjs')).default
-  console.log('Lightning Client: ',LightningClient)
-  const lightning_client = new LightningClient()
-  return lightning_client
-}
+// Routes
+app.use("/", getRoutes);
+app.use("/", postRoutes);
 
-const ldk = importLDK();
-
+// Starting the server
 app.listen(PORT, () => {
-  // log(
-  //   "info",
-  //   `mercury-wallet-LDK-adapter listening at http://localhost:${PORT}`
-  // );
-  console.log(
-    `mercury-wallet-LDK-adapter listening at http://localhost:${PORT}`
-  )
-  // log("info", `${network} data dir` + dataDir);
+  console.log(`lightning-adapter listening at http://localhost:${PORT}`);
 });
 
+// Import LDK
+importLDK();
 
-app.post("/lightning/generate_invoice", async function (req, res) {
-  try {
-    let invoice_config = {
-      amt_in_sats: req.body.amt_in_sats,
-      invoice_expiry_secs: req.body.invoice_expiry_secs,
-      description: req.body.description,
-    }
-    let invoice = (await ldk).create_invoice(invoice_config.amt_in_sats, invoice_config.invoice_expiry_secs, invoice_config.description);
-    // log("info", `Generating Invoice`);
-    let response = invoice;
-    console.log(response)
-    res.status(200).json(response);
-  } catch (err) {
-    const err_msg = `Bad request: ${err}`;
-    console.log(err_msg)
-    // log("error", err_msg);
-    // log("info", `info - ${err_msg}`);
-    // handle_error(res, err);
-  }
-});
+// Exit handlers
+const onExit = () => {
+  // code to be executed on exit, e.g. close connections, cleanup resources
+  console.log("Exiting the application");
+};
 
-
-async function on_exit() {
-  // if (logger) {
-  //   log("info", `on_exit - stopping tor node...`);
-  // }
-  // await anon_client.stopTorNode();
-}
-
-async function on_sig_int() {
-  // if (logger) {
-  //   log("info", `on_sig_int - exiting...`);
-  // }
+const onSigInt = () => {
+  // code to be executed on sigint, e.g. close connections, cleanup resources
+  console.log("Application interrupted");
   process.exit();
-}
+};
 
-process.on("exit", on_exit);
-process.on("SIGINT", on_sig_int);
+process.on("exit", onExit);
+process.on("SIGINT", onSigInt);
