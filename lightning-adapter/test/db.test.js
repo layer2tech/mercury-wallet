341bd065ea8fcb883742fb78093e4b71c8892d66
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-01-12 16:03:11 +0000

    chore: add db test on channels

--- lightning-adapter/test/db.test.js
+++ lightning-adapter/test/db.test.js
@@ -9,3 +9,13 @@ test("Check that sample data is in the wallets table", (done) => {
     done();
   });
 });
+
+test("Check that sample data is in the channels table", (done) => {
+  db.all("SELECT * FROM channels", (err, rows) => {
+    expect(rows.length).toBe(3);
+    expect(rows[0].name).toBe("channel1");
+    expect(rows[1].name).toBe("testChannel");
+    expect(rows[2].name).toBe("p2p");
+    done();
+  });
+});
