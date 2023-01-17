234a4c60e386a7f8ad9341ae435efc6e79f05f5d
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-01-17 09:11:42 +0000

    chore: add event handler for creating channel

--- lightning-adapter/lightning/MercuryEventHandler.mjs
+++ lightning-adapter/lightning/MercuryEventHandler.mjs
@@ -1,4 +1,7 @@
-import { Event, EventHandler, EventsProvider, 
+import {
+  Event,
+  EventHandler,
+  EventsProvider,
   Event_FundingGenerationReady,
   Event_PaymentReceived,
   Event_PaymentSent,
@@ -6,20 +9,20 @@ import { Event, EventHandler, EventsProvider,
   Event_PendingHTLCsForwardable,
   Event_SpendableOutputs,
   Event_PaymentForwarded,
-  Event_ChannelClosed } from "lightningdevkit";
+  Event_ChannelClosed,
+  Event_OpenChannelRequest,
+} from "lightningdevkit";
 
-
-class MercuryEventHandler{
-  constructor(callbackFn){
+class MercuryEventHandler {
+  constructor(callbackFn) {
     // super(callbackFn);
-    this.callbackFn = callbackFn
+    this.callbackFn = callbackFn;
   }
 
   handle_event(e) {
-    
     console.log(">>>>>>> Handling Event here <<<<<<<", e);
     if (e instanceof Event_FundingGenerationReady) {
-      this.callbackFn(e)
+      this.callbackFn(e);
       // console.log('Event Funding Generation Ready!!')
       //console.log(e)
       // var final_tx = generateFundingTransaction(e.output_script, e.channel_value_satoshis);
@@ -27,13 +30,13 @@ class MercuryEventHandler{
 
       // // console.log( 'Funding Generation Event: ',e );
       // // console.log('THIS: ', this);
-      
+
       // this.channel_manager.funding_transaction_generated(e.temporary_channel_id, e.counterparty_node_id, final_tx);
       // //channel_manager.funding_transaction_generated(e.temporary_channel_id, e.counterparty_node_id, final_tx);
       // // <insert code to handle this event>
     } else if (e instanceof Event_PaymentReceived) {
       // Handle successful payment
-      
+
       assert(event.payment_preimage instanceof Option.payment_preimage);
       const event = e;
       const payment_preimage = event.payment_preimage;
@@ -49,6 +52,7 @@ class MercuryEventHandler{
       // <insert code to handle this event>
     } else if (e instanceof Event_PaymentForwarded) {
       // <insert code to handle this event>
+    } else if (e instanceof Event_OpenChannelRequest) {
     } else if (e instanceof Event_ChannelClosed) {
       // <insert code to handle this event>
     }
