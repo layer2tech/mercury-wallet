6e3f4a9c371814db566940eed1d82ec0d563ad9c
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-01-27 18:00:36 +0000

    chore: cleanup

--- lightning-adapter/lightning/MercuryEventHandler.mjs
+++ lightning-adapter/lightning/MercuryEventHandler.mjs
@@ -79,66 +79,6 @@ class MercuryEventHandler {
     );
   }
 
-  /*
-  handleFundingGenerationReadyEvent(event) {
-    const fundingScriptPubkey = event.output_script;
-    const outputValue = event.channel_value_satoshis;
-
-    // Check that the output is a P2WSH
-    
-    if (
-      fundingScriptPubkey.length !== 34 &&
-      fundingScriptPubkey[0] !== 0 &&
-      fundingScriptPubkey[1] !== 32
-    ) {
-      return;
-    }
-
-    console.assert(
-      funding_scriptpubkey.length == 34 &&
-        funding_scriptpubkey[0] == 0 &&
-        funding_scriptpubkey[1] == 32,
-      "Check that the output is a P2WSH"
-    );
-
-    // Create a new transaction
-    const fundingTx = new bitcoin.Transaction();
-
-    // Add input to the transaction
-    fundingTx.addInput(
-      new bitcoin.Transaction.Input({
-        prevTxId: funding,
-        script: new bitcoin.Script(),
-      })
-    );
-
-    // Add witness to the input
-    fundingTx.inputs[0].witness.push(Buffer.from([0x1]));
-
-    // Add output to the transaction
-    fundingTx.addOutput(
-      bitcoin.Script.fromBuffer(fundingScriptPubkey),
-      outputValue
-    );
-
-    // let funding_res = new Result_NoneAPIErrorZ;
-
-    // Give the funding transaction back to the ChannelManager
-    let funding_res = this.channel_manager.funding_transaction_generated(
-      event.temporary_channel_id,
-      fundingTx.toHex()
-    );
-
-    // funding_transaction_generated should only generate an error if the
-    // transaction didn't meet the required format (or the counterparty already
-    // closed the channel on us):
-
-    console.assert(
-      funding_res instanceof Result_NoneAPIErrorZ_OK,
-      "funding_transaction_generated did not meet the required format or the counterparty already closed the channel"
-    );
-  }*/
-
   handlePaymentReceivedEvent(e) {
     console.log(`Payment of ${e.amt} SAT received.`);
     this.channelManager.claim_funds(e.payment_preimage);
