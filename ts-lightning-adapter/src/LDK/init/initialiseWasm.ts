98b3bc266454d6ce5e979d2883990b9e581946ee
Sam Gershuny <samgershuny@gmail.com>
2023-02-06 17:16:27 +0000

    FEAT: TS LDK Startup Script

--- ts-lightning-adapter/src/LDK/init/initialiseWasm.ts
+++ ts-lightning-adapter/src/LDK/init/initialiseWasm.ts
@@ -2,8 +2,14 @@ import * as ldk from 'lightningdevkit';
 import fs from 'fs';
 
 export default async function initialiseWasm() {
+  try{
+    //"/Users/samgershuny/Documents/mercury-wallet/ts-lightning-adapter/node_modules/lightningdevkit/liblightningjs.wasm"
     const wasm_file = fs.readFileSync(
         "node_modules/lightningdevkit/liblightningjs.wasm"
       );
     await ldk.initializeWasmFromBinary(wasm_file);
+
+  } catch(e){
+    throw new Error(`InitialiseWasmError: ${e}`)
+  }
 }
