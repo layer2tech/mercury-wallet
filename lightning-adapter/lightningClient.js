3259a1b42f27068712c2f0ccf45e1ab58de59947
Sam Gershuny <samgershuny@gmail.com>
2023-02-24 11:27:38 +0000

    HOTFIX: Remove Lightning Files

--- lightning-adapter/lightningClient.js
+++
@@ -1,8 +0,0 @@
-let LDKClient;
-importLDK = async () => {
-  const { default: LightningClient } = await import("./lightning.mjs");
-  LDKClient = new LightningClient();
-  console.log("Lightning Client: ", LightningClient);
-};
-
-module.exports = { importLDK, LDKClient };
