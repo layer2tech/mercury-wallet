80415bba1a498cc0e1481fa01d417121daee5b33
Dhananjay Purohit <dhananjaypurohit7@gmail.com>
2023-05-15 21:32:10 +0530

    feat: send lightning changes (#2002)

--- src/features/WalletDataSlice.js
+++ src/features/WalletDataSlice.js
@@ -200,6 +200,12 @@ export const callGetChannels = async (wallet_name) => {
   }
 };
 
+export const callSendPayment = async (invoice_string) => {
+  if (isWalletLoaded()) {
+    return await wallet.lightning_client.sendPayment({invoice: invoice_string});
+  }
+}
+
 export const getTotalChannelBalance = () => {
   if (isWalletLoaded()) {
     return wallet.channels.getTotalChannelBalance();
