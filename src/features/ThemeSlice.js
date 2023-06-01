edde07a09e1229939b20962d6782693272698cc4
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-06-01 15:54:35 +0100

    chore: check localStorage

--- src/features/ThemeSlice.js
+++ src/features/ThemeSlice.js
@@ -2,7 +2,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  dark_mode: window.darkMode,
+  dark_mode:
+    localStorage ?? localStorage.getItem("dark_mode") != null
+      ? localStorage.getItem("dark_mode")
+      : null,
 };
 
 const ThemeSlice = createSlice({
@@ -10,12 +13,25 @@ const ThemeSlice = createSlice({
   initialState,
   reducers: {
     toggleDarkMode(state) {
-      state.dark_mode = !state.dark_mode;
-      window.darkMode = state.dark_mode;
-      return state;
+      return {
+        ...state,
+        dark_mode: calculateDarkmode(state),
+      };
     },
   },
 });
+
+const calculateDarkmode = (state) => {
+  if (state.dark_mode === "1") {
+    localStorage.removeItem("dark_mode");
+    window.darkMode.off();
+    return "0";
+  } else {
+    localStorage.setItem("dark_mode", "1");
+    window.darkMode.on();
+    return "1";
+  }
+};
 
 export const { toggleDarkMode } = ThemeSlice.actions;
 export default ThemeSlice.reducer;
