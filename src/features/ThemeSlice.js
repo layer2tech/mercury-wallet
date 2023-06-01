0ed1b03b0e8c899209780debec7def8c68df00ed
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-06-01 14:54:31 +0100

    chore: change theme slice

--- src/features/ThemeSlice.js
+++ src/features/ThemeSlice.js
@@ -2,10 +2,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  dark_mode:
-    localStorage.getItem("dark_mode") != null
-      ? localStorage.getItem("dark_mode")
-      : null,
+  dark_mode: window.darkMode,
 };
 
 const ThemeSlice = createSlice({
@@ -13,25 +10,12 @@ const ThemeSlice = createSlice({
   initialState,
   reducers: {
     toggleDarkMode(state) {
-      return {
-        ...state,
-        dark_mode: calculateDarkmode(state),
-      };
+      state.dark_mode = !state.dark_mode;
+      window.darkMode = state.dark_mode;
+      return state;
     },
   },
 });
-
-const calculateDarkmode = (state) => {
-  if (state.dark_mode === "1") {
-    localStorage.removeItem("dark_mode");
-    window.darkMode.off();
-    return "0";
-  } else {
-    localStorage.setItem("dark_mode", "1");
-    window.darkMode.on();
-    return "1";
-  }
-};
 
 export const { toggleDarkMode } = ThemeSlice.actions;
 export default ThemeSlice.reducer;
