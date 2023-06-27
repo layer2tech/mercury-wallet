3b95982c282d2a4d58c7cdd78d01955d7dcf5e7c
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-06-27 09:13:00 +0100

    chore: fix

--- lightning-adapter/src/LDK/init/initializeWasm.ts
+++ lightning-adapter/src/LDK/init/initializeWasm.ts
@@ -1,11 +1,19 @@
 import * as ldk from "lightningdevkit";
 import fs from "fs";
+import path from "path";
 
 export default async function initializeWasm() {
   try {
-    const wasm_file = await fs.promises.readFile(
-      "../lightningdevkit/liblightningjs.wasm"
-    );
+    let wasm_path;
+    if (process.platform === "darwin") {
+      wasm_path = path.resolve(
+        "../node_modules/lightningdevkit/liblightningjs.wasm"
+      );
+    } else {
+      wasm_path = path.resolve("../lightningdevkit/liblightningjs.wasm");
+    }
+
+    let wasm_file = await fs.promises.readFile(wasm_path);
 
     await ldk.initializeWasmFromBinary(wasm_file);
   } catch (e) {
