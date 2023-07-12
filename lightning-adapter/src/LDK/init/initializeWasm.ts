2ea6940f3e3f2639426dbdcc259e68c20ef08f4e
RajKadir <r.kadir@se16.qmul.ac.uk>
2023-07-12 12:59:53 +0100

    chore: add initWasm Change

--- lightning-adapter/src/LDK/init/initializeWasm.ts
+++ lightning-adapter/src/LDK/init/initializeWasm.ts
@@ -5,18 +5,18 @@ import path from "path";
 export default async function initializeWasm() {
   try {
     let wasm_path;
-    if (process.platform === "darwin") {
-      wasm_path = path.resolve(
-        "../node_modules/lightningdevkit/liblightningjs.wasm"
-      );
-    } else {
+    /* USE THIS FOR WINDOWS DEVELOPMENT IF BELOW DOESN'T WORK
+    const args = process.argv.slice(2); // Get command line arguments
+    if (args.includes("dev-windows")) {
       wasm_path = path.resolve("../lightningdevkit/liblightningjs.wasm");
-    }
+    }*/
 
+    wasm_path = path.resolve(
+      "../node_modules/lightningdevkit/liblightningjs.wasm"
+    );
     let wasm_file = await fs.promises.readFile(wasm_path);
-
     await ldk.initializeWasmFromBinary(wasm_file);
   } catch (e) {
-    throw new Error(`[initialiseWasm.ts]: InitialiseWasmError: ${e}`);
+    throw new Error(`[initializeWasm.ts]: InitializeWasmError: ${e}`);
   }
 }
